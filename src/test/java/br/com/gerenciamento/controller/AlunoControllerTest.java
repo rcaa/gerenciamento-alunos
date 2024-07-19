158d20880a22e405e410adf2deba6e8a36fc5162
Marcos Vinícius Cavalcante Oliveira <mvinic2011@gmail.com>
2024-07-18 23:54:22 -0300

    testes de controller

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,86 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.service.ServiceAluno;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ServiceAluno serviceAluno;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testInsertAlunos() throws Exception {
+        mockMvc.perform(get("/inserirAlunos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/formAluno"))
+                .andExpect(model().attributeExists("aluno"));
+    }
+
+    @Test
+    public void testListagemAlunos() throws Exception {
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Vinicius");
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Carlos");
+
+        Mockito.when(serviceAluno.findAll()).thenReturn(Arrays.asList(aluno1, aluno2));
+
+        mockMvc.perform(get("/alunos-adicionados"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/listAlunos"))
+                .andExpect(model().attributeExists("alunosList"));
+    }
+
+    @Test
+    public void testEditar() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+
+        Mockito.when(serviceAluno.getById(1L)).thenReturn(aluno);
+
+        mockMvc.perform(get("/editar/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/editar"))
+                .andExpect(model().attributeExists("aluno"))
+                .andExpect(model().attribute("aluno", aluno));
+    }
+
+    @Test
+    public void testRemoverAluno() throws Exception {
+        mockMvc.perform(get("/remover/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/alunos-adicionados"));
+
+        Mockito.verify(serviceAluno, Mockito.times(1)).deleteById(1L);
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,95 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.service.ServiceUsuario;
+import br.com.gerenciamento.util.Util;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ServiceUsuario serviceUsuario;
+
+    private MockHttpSession session;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        session = new MockHttpSession();
+    }
+
+    @Test
+    public void testLoginPage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"))
+                .andExpect(model().attributeExists("usuario"));
+    }
+
+    @Test
+    public void testIndexPage() throws Exception {
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home/index"))
+                .andExpect(model().attributeExists("aluno"));
+    }
+
+    @Test
+    public void testCadastrarUsuario() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("testUser");
+        usuario.setSenha("testPassword");
+
+        mockMvc.perform(post("/salvarUsuario")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("user", usuario.getUser())
+                        .param("senha", usuario.getSenha()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        Mockito.verify(serviceUsuario, Mockito.times(1)).salvarUsuario(Mockito.any(Usuario.class));
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("testUser");
+        usuario.setSenha("testPassword");
+
+        Mockito.when(serviceUsuario.loginUser(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(usuario);
+
+        mockMvc.perform(post("/login")
+                        .session(session)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("user", usuario.getUser())
+                        .param("senha", usuario.getSenha()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home/index"))
+                .andExpect(model().attributeExists("aluno"));
+
+        Mockito.verify(serviceUsuario, Mockito.times(1)).loginUser(Mockito.anyString(), Mockito.anyString());
+    }
 }
