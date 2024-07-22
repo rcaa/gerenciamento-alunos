32f317e1635a9278f24dacd93dcccb0804a2f870
Capeudinho <archmagepyromancer@gmail.com>
2024-07-22 01:56:17 -0300

    Implementação de testes de UsuarioRepositoryTest e atualização de AlunoRepositoryTest

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,14 +1,12 @@
 package br.com.gerenciamento.repository;
 
 import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import br.com.gerenciamento.enums.Curso;
 import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,68 @@
 package br.com.gerenciamento.repository;
 
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import br.com.gerenciamento.model.Usuario;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolationException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UsuarioRepositoryTest {
+public class UsuarioRepositoryTest
+{
+	@Autowired
+    private UsuarioRepository UsuarioRepository;
+
+	@Test
+	@Transactional
+    public void save()
+	{
+		Usuario usuario = new Usuario();
+		usuario.setEmail("aaaaa@a.a");
+        usuario.setUser("aaaaa");
+        usuario.setSenha("aaaaa");
+		this.UsuarioRepository.save(usuario);
+		List<Usuario> usuarios = this.UsuarioRepository.findAll();
+		Assert.assertTrue(usuarios.get(0).getUser().equals("aaaaa"));
+	}
+
+	@Test
+	@Transactional
+    public void saveInvalidEmail()
+	{
+		Usuario usuario = new Usuario();
+		usuario.setEmail("aaaaa");
+        usuario.setUser("aaaaa");
+        usuario.setSenha("aaaaa");
+		Assert.assertThrows(ConstraintViolationException.class, () -> {this.UsuarioRepository.save(usuario);});
+	}
+
+	@Test
+	@Transactional
+    public void saveInvalidUser()
+	{
+		Usuario usuario = new Usuario();
+		usuario.setEmail("aaaaa@.a");
+        usuario.setUser("a");
+        usuario.setSenha("aaaaa");
+		Assert.assertThrows(ConstraintViolationException.class, () -> {this.UsuarioRepository.save(usuario);});
+	}
+
+	@Test
+	@Transactional
+    public void buscarLogin()
+	{
+		Usuario usuario = new Usuario();
+		usuario.setEmail("aaaaa@a.a");
+        usuario.setUser("aaaaa");
+        usuario.setSenha("aaaaa");
+		this.UsuarioRepository.save(usuario);
+		Usuario usuarioAchado = this.UsuarioRepository.buscarLogin("aaaaa", "aaaaa");
+		Assert.assertTrue(usuarioAchado.getEmail().equals("aaaaa@a.a"));
+	}
 }
