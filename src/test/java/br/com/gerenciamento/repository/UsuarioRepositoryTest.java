3155acb2ff46f584682031e280805fe3433b42aa
Gustavo Barbosa <gustavobarbosa-18@hotmail.com>
2024-07-27 18:01:14 -0300

    test(UsuarioRepository): validar todo o repository

--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,59 @@
 package br.com.gerenciamento.repository;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import br.com.gerenciamento.model.Usuario;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    private Usuario usuario;
+
+    @BeforeEach
+    void setUp() {
+        usuario = new Usuario();
+        usuario.setEmail("test@example.com");
+        usuario.setUser("testuser");
+        usuario.setSenha("password");
+        usuarioRepository.save(usuario);
+    }
+
+    @Test
+    public void findByEmail() {
+        Usuario foundUsuario = usuarioRepository.findByEmail("test@example.com");
+        assertNotNull(foundUsuario);
+        assertEquals("testuser", foundUsuario.getUser());
+    }
+
+    @Test
+    public void buscarLogin() {
+        Usuario foundUsuario = usuarioRepository.buscarLogin("testuser", "password");
+        assertNotNull(foundUsuario);
+        assertEquals("test@example.com", foundUsuario.getEmail());
+    }
+
+    @Test
+    public void findByEmailNotFound() {
+        Usuario foundUsuario = usuarioRepository.findByEmail("nonexistent@example.com");
+        assertNull(foundUsuario);
+    }
+
+    @Test
+    public void buscarLoginInvalidCredentials() {
+        Usuario foundUsuario = usuarioRepository.buscarLogin("testuser", "wrongpassword");
+        assertNull(foundUsuario);
+
+        foundUsuario = usuarioRepository.buscarLogin("wronguser", "password");
+        assertNull(foundUsuario);
+    }
 }
