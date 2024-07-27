26425718f8250eadc670ba2dce27a77c9f0b94ba
Gustavo Barbosa <gustavobarbosa-18@hotmail.com>
2024-07-27 17:40:20 -0300

    test(AlunoService): validar todo o serviço

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -1,47 +1,97 @@
 package br.com.gerenciamento.service;
 
-import br.com.gerenciamento.enums.Curso;
-import br.com.gerenciamento.enums.Status;
-import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
-import jakarta.validation.ConstraintViolationException;
-import org.junit.*;
+import br.com.gerenciamento.repository.AlunoRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoServiceTest {
 
-    @Autowired
+    @InjectMocks
     private ServiceAluno serviceAluno;
 
-    @Test
-    public void getById() {
-        Aluno aluno = new Aluno();
+    @Mock
+    private AlunoRepository alunoRepository;
+
+    private Aluno aluno;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        aluno = new Aluno();
         aluno.setId(1L);
         aluno.setNome("Vinicius");
-        aluno.setTurno(Turno.NOTURNO);
-        aluno.setCurso(Curso.ADMINISTRACAO);
-        aluno.setStatus(Status.ATIVO);
-        aluno.setMatricula("123456");
-        this.serviceAluno.save(aluno);
-
-        Aluno alunoRetorno = this.serviceAluno.getById(1L);
-        Assert.assertTrue(alunoRetorno.getNome().equals("Vinicius"));
+        // Configure outros atributos de aluno conforme necessário
     }
 
     @Test
-    public void salvarSemNome() {
-        Aluno aluno = new Aluno();
-        aluno.setId(1L);
-        aluno.setTurno(Turno.NOTURNO);
-        aluno.setCurso(Curso.ADMINISTRACAO);
-        aluno.setStatus(Status.ATIVO);
-        aluno.setMatricula("123456");
-        Assert.assertThrows(ConstraintViolationException.class, () -> {
-                this.serviceAluno.save(aluno);});
+    public void save() {
+        serviceAluno.save(aluno);
+        verify(alunoRepository, times(1)).save(aluno);
+    }
+
+    @Test
+    public void findAll() {
+        when(alunoRepository.findAll()).thenReturn(Arrays.asList(aluno));
+        List<Aluno> alunos = serviceAluno.findAll();
+        assertNotNull(alunos);
+        assertEquals(1, alunos.size());
+        assertEquals("Vinicius", alunos.get(0).getNome());
+    }
+
+    @Test
+    public void getById() {
+        when(alunoRepository.findById(1L)).thenReturn(Optional.of(aluno));
+        Aluno alunoRetorno = serviceAluno.getById(1L);
+        assertNotNull(alunoRetorno);
+        assertEquals("Vinicius", alunoRetorno.getNome());
+    }
+
+    @Test
+    public void deleteById() {
+        serviceAluno.deleteById(1L);
+        verify(alunoRepository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    public void findByStatusAtivo() {
+        when(alunoRepository.findByStatusAtivo()).thenReturn(Arrays.asList(aluno));
+        List<Aluno> alunos = serviceAluno.findByStatusAtivo();
+        assertNotNull(alunos);
+        assertEquals(1, alunos.size());
+        assertEquals("Vinicius", alunos.get(0).getNome());
+    }
+
+    @Test
+    public void findByStatusInativo() {
+        when(alunoRepository.findByStatusInativo()).thenReturn(Arrays.asList(aluno));
+        List<Aluno> alunos = serviceAluno.findByStatusInativo();
+        assertNotNull(alunos);
+        assertEquals(1, alunos.size());
+        assertEquals("Vinicius", alunos.get(0).getNome());
+    }
+
+    @Test
+    public void findByNomeContainingIgnoreCase() {
+        when(alunoRepository.findByNomeContainingIgnoreCase("vinicius")).thenReturn(Arrays.asList(aluno));
+        List<Aluno> alunos = serviceAluno.findByNomeContainingIgnoreCase("vinicius");
+        assertNotNull(alunos);
+        assertEquals(1, alunos.size());
+        assertEquals("Vinicius", alunos.get(0).getNome());
     }
 }
