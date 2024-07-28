f1f08fa6094d5db749a00c0fb1f20f08d5ef291c
pedro-galdino <pedro.galdino34@gmail.com>
2024-07-28 01:37:35 -0300

    Adicionando testes de aceitação utilizando Selenium

---
+++ src/test/java/br/com/gerenciamento/acceptance/UsuarioAcceptanceTest.java
@@ -0,0 +1,64 @@
+package br.com.gerenciamento.acceptance;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.GerenciamentoAlunosApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsuarioAcceptanceTest {
+
+	private WebDriver driver;
+	private ConfigurableApplicationContext context;
+
+	@Before
+	public void setup() {
+		context = SpringApplication.run(GerenciamentoAlunosApplication.class);
+
+		driver = new ChromeDriver();
+		driver.get("http://localhost:8080");
+	}
+
+	@After
+	public void teardown() {
+		driver.quit();
+		context.close();
+	}
+
+	@Test
+	public void cadastrarELogarUsuario() {
+		String email = "pedro@gmail.com";
+		String user = "pedro";
+		String senha = "1234";
+
+		driver.findElement(By.linkText("Clique aqui para se cadastrar")).click();
+
+		// cadastro
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.id("user")).sendKeys(user);
+		driver.findElement(By.id("senha")).sendKeys(senha);
+		driver.findElement(By.className("btn-primary")).click();
+
+		// login
+		driver.findElement(By.id("user")).sendKeys(user);
+		driver.findElement(By.id("senha")).sendKeys(senha);
+		driver.findElement(By.className("btn-primary")).click();
+
+		String expectedText = "Sistema de Gerenciamento de Alunos";
+		String actualText = driver.findElement(By.className("texto-centro")).getText();
+
+		Assert.assertEquals(expectedText, actualText);
+	}
+
+}
