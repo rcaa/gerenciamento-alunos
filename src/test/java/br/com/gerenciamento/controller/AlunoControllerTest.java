48ddbdb83f97049db54e7f083e1079fbd8ac7cd1
marco_antonio <marcoantoniomoraes760@gmail.com>
2024-07-28 19:41:50 -0300

    test: Definir testes de integração para AlunoController - UsuarioController

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,102 @@
 package br.com.gerenciamento.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import jakarta.validation.ConstraintViolationException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoControllerTest {
+
+    @Autowired
+    private AlunoController alunoController;
+
+    @Test
+    public void inserirAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setMatricula("123456");
+        aluno.setNome("Marco");
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.INFORMATICA);
+
+        BindingResult bindingResult = new BeanPropertyBindingResult(aluno, "aluno");
+
+        ModelAndView modelAndView = this.alunoController.inserirAluno(aluno, bindingResult);
+
+        assertEquals("redirect:/alunos-adicionados", modelAndView.getViewName());
+    }
+
+    @Test
+    public void inserirAlunoSemMatricula() {
+        Aluno aluno = new Aluno();
+        aluno.setId(2L);
+        aluno.setMatricula("");
+        aluno.setNome("Maria Zuleide");
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.BIOMEDICINA);
+
+        BindingResult bindingResult = new BeanPropertyBindingResult(aluno, "aluno");
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            this.alunoController.inserirAluno(aluno, bindingResult);
+        });
+    }
+
+    @Test
+    public void editarAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setId(3L);
+        aluno.setMatricula("776655");
+        aluno.setNome("Maria do Carmo");
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.BIOMEDICINA);
+        BindingResult bindingResult = new BeanPropertyBindingResult(aluno, "aluno");
+        this.alunoController.inserirAluno(aluno, bindingResult);
+
+        aluno.setMatricula("225577");
+        aluno.setNome("Maria do Carmo Silva");
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ENFERMAGEM);
+
+        ModelAndView modelAndView = this.alunoController.editar(aluno);
+
+        assertEquals("redirect:/alunos-adicionados", modelAndView.getViewName());
+    }
+
+    @Test
+    public void removerAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setId(4L);
+        aluno.setMatricula("992299");
+        aluno.setNome("Mario Silva");
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        BindingResult bindingResult = new BeanPropertyBindingResult(aluno, "aluno");
+        this.alunoController.inserirAluno(aluno, bindingResult);
+
+        String modelAndView = this.alunoController.removerAluno(aluno.getId());
+
+        assertEquals("redirect:/alunos-adicionados", modelAndView);
+    }
+
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,78 @@
 package br.com.gerenciamento.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.gerenciamento.model.Usuario;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.ConstraintViolationException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioControllerTest {
+
+    @Autowired
+    private UsuarioController usuarioController;
+
+    @Test
+    public void cadastrar() throws Exception{
+        Usuario user = new Usuario();
+        user.setId(1L);
+        user.setEmail("teste@email.com");
+        user.setUser("teste");
+        user.setSenha("teste123");
+
+        ModelAndView modelAndView = this.usuarioController.cadastrar(user);
+
+        assertEquals("redirect:/", modelAndView.getViewName());
+    }
+
+    @Test
+    public void cadastrarUsuarioSemUserName() {
+        Usuario user = new Usuario();
+        user.setId(2L);
+        user.setEmail("teste2@email.com");
+        user.setUser("");
+        user.setSenha("teste321");
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            this.usuarioController.cadastrar(user);
+        });
+    }
+
+    @Test
+    public void loginComUsuarioInexistente() throws Exception {
+        Usuario user = new Usuario();
+        user.setId(2L);
+        user.setEmail("teste2@email.com");
+        user.setUser("teste2");
+        user.setSenha("teste321");
+
+        HttpSession mockSession = Mockito.mock(HttpSession.class);
+        BindingResult bindingResult = new BeanPropertyBindingResult(user, "usuario");
+
+        ModelAndView modelAndView = this.usuarioController.login(user, bindingResult, mockSession);
+
+        assertEquals("login/cadastro", modelAndView.getViewName());
+    }
+
+    @Test
+    public void logout() {
+        HttpSession mockSession = Mockito.mock(HttpSession.class);
+
+        ModelAndView modelAndView = this.usuarioController.logout(mockSession);
+
+        assertEquals("login/login", modelAndView.getViewName());
+    }
+
 }
