8d2af71833ab16db3643cb1edaf9fb041e707a30
Rai Araujo <raisanhacu@gmail.com>
2024-07-27 23:14:41 -0300

    Adicionando testes de integração nas classes controladoras

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,53 @@
 package br.com.gerenciamento.controller;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testInsertAlunos() throws Exception {
+        mockMvc.perform(get("/inserirAlunos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/formAluno"))
+                .andExpect(model().attributeExists("aluno"));
+    }
+    
+
+    @Test
+    public void testRemoverAluno() throws Exception {
+        // Supondo que um aluno com ID 1 existe no banco de dados para fins de teste
+        mockMvc.perform(get("/remover/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/alunos-adicionados"));
+    }
+
+    @Test
+    public void testFiltroAlunos() throws Exception {
+        mockMvc.perform(get("/filtro-alunos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/filtroAlunos"));
+    }
+
+    @Test
+    public void testListaAlunosAtivos() throws Exception {
+        mockMvc.perform(get("/alunos-ativos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/alunos-ativos"))
+                .andExpect(model().attributeExists("alunosAtivos"));
+    }
+
+    
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,56 @@
 package br.com.gerenciamento.controller;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testLogin() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"))
+                .andExpect(model().attributeExists("usuario"));
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home/index"))
+                .andExpect(model().attributeExists("aluno"));
+    }
+
+    @Test
+    public void testCadastrar() throws Exception {
+        mockMvc.perform(get("/cadastro"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/cadastro"))
+                .andExpect(model().attributeExists("usuario"));
+    }
+
+    @Test
+    public void testSalvarUsuario() throws Exception {
+        mockMvc.perform(post("/salvarUsuario")
+                        .param("email", "test@test.com")
+                        .param("user", "testUser")
+                        .param("senha", "password"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
 }
