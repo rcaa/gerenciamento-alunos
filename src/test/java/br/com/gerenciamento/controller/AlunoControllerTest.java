587b37f1d312fabc619456a4b0987ddd8c8d3e1d
ricaellepontes <166752065+ricaellepontes@users.noreply.github.com>
2024-07-28 19:18:00 -0300

    Implementação de testes de integração para `UsuarioController` e `AlunoController'
    - `UsuarioControllerTest`
      - Adicionado teste `testCadastroUsuario`: Verifica o cadastro bem-sucedido de um usuário e o redirecionamento para a página inicial.
      - Adicionado teste `testLoginUsuarioValido`: Testa o login com credenciais válidas e o redirecionamento para a página principal.
      - Adicionado teste `testLoginUsuarioInvalido`: Testa o login com credenciais inválidas e a exibição da mensagem de erro apropriada na página de login.
      - Adicionado teste `testLogoutUsuario`: Verifica o redirecionamento para a página inicial após o logout do usuário.
    
    - `AlunoControllerTest`:
      - Adicionado teste `testCreateAluno`: Verifica a criação bem-sucedida de um aluno e o redirecionamento para a página de alunos adicionados.
      - Adicionado teste `testGetAlunoById`: Testa a recuperação de um aluno pelo ID e a exibição correta na página de edição.
      - Adicionado teste `testUpdateAluno`: Verifica a atualização bem-sucedida de um aluno e o redirecionamento para a página de alunos adicionados.
      - Adicionado teste `testDeleteAluno`: Testa a exclusão de um aluno pelo ID e o redirecionamento para a página de alunos adicionados.

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,105 @@
 package br.com.gerenciamento.controller;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.service.ServiceAluno;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AlunoController.class)
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ServiceAluno serviceAluno;
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testCreateAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Ana Costa");
+        aluno.setCurso(Curso.ENFERMAGEM);
+        aluno.setMatricula("11223");
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.NOTURNO);
+
+        doNothing().when(serviceAluno).save(any(Aluno.class));
+
+        mockMvc.perform(post("/InsertAlunos")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("nome", aluno.getNome())
+                .param("curso", aluno.getCurso().toString())
+                .param("matricula", aluno.getMatricula())
+                .param("status", aluno.getStatus().toString())
+                .param("turno", aluno.getTurno().toString()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/alunos-adicionados"));
+    }
+
+
+
+    @Test
+    public void testGetAlunoById() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Ana Costa");
+
+        when(serviceAluno.getById(1L)).thenReturn(aluno);
+
+        mockMvc.perform(get("/editar/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/editar"))
+                .andExpect(model().attributeExists("aluno"))
+                .andExpect(model().attribute("aluno", aluno));
+    }
+
+    @Test
+    public void testUpdateAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Ana Costa");
+        aluno.setCurso(Curso.ENFERMAGEM);
+        aluno.setMatricula("11223");
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.NOTURNO);
+
+
+        doNothing().when(serviceAluno).save(any(Aluno.class));
+
+       
+        mockMvc.perform(post("/editar")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(aluno)))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/alunos-adicionados"));
+    }
+
+    @Test
+    public void testDeleteAluno() throws Exception {
+        doNothing().when(serviceAluno).deleteById(1L);
+
+        mockMvc.perform(get("/remover/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/alunos-adicionados"));
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,96 @@
 package br.com.gerenciamento.controller;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.service.ServiceUsuario;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UsuarioController.class)
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private ServiceUsuario serviceUsuario;
+
+    @InjectMocks
+    private UsuarioController usuarioController;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(usuarioController).build();
+    }
+
+    @Test
+    public void testCadastroUsuario() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario");
+        usuario.setSenha("senha");
+
+        doNothing().when(serviceUsuario).salvarUsuario(any(Usuario.class));
+
+        mockMvc.perform(post("/salvarUsuario")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("user", usuario.getUser())
+                .param("senha", usuario.getSenha()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void testLoginUsuarioValido() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario");
+        usuario.setSenha("senha");
+
+        when(serviceUsuario.loginUser(anyString(), anyString())).thenReturn(usuario);
+
+        mockMvc.perform(post("/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("user", usuario.getUser())
+                .param("senha", usuario.getSenha()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/index"));
+    }
+
+    @Test
+    public void testLoginUsuarioInvalido() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario");
+        usuario.setSenha("senha");
+
+        when(serviceUsuario.loginUser(anyString(), anyString())).thenReturn(null);
+
+        mockMvc.perform(post("/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("user", usuario.getUser())
+                .param("senha", usuario.getSenha()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"))
+                .andExpect(model().attribute("msg", "Usuario não encontrado. Tente novamente"));
+    }
+
+    @Test
+    public void testLogoutUsuario() throws Exception {
+        mockMvc.perform(post("/logout"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
 }
