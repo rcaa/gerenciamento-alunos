768e419290bbec20480405746dfa0124917cfc1e
Rai Araujo <raisanhacu@gmail.com>
2024-07-27 23:13:47 -0300

    Adicionando testes unitarios nas classes repositorys

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,10 +1,83 @@
 package br.com.gerenciamento.repository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class AlunoRepositoryTest {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Test
+    public void testFindByStatusAtivo() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        alunoRepository.save(aluno);
+
+        List<Aluno> alunosAtivos = alunoRepository.findByStatusAtivo();
+        assertFalse(alunosAtivos.isEmpty());
+    }
+
+    @Test
+    public void testFindByStatusInativo() {
+
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.INATIVO);
+        aluno.setMatricula("123456");
+        alunoRepository.save(aluno);
+
+        List<Aluno> alunosInativos = alunoRepository.findByStatusInativo();
+        assertFalse(alunosInativos.isEmpty());
+    }
+
+    @Test
+    public void testFindByNomeContainingIgnoreCase() {
+        
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        alunoRepository.save(aluno);
+
+        List<Aluno> alunos = alunoRepository.findByNomeContainingIgnoreCase("Vinicius");
+        assertFalse(alunos.isEmpty());
+    }
+
+    @Test
+    public void testSave() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Novo Aluno");
+        aluno.setMatricula("123");
+        aluno.setCurso(Curso.ADMINISTRACAO); 
+        aluno.setStatus(Status.ATIVO); 
+        aluno.setTurno(Turno.MATUTINO); 
+
+        Aluno savedAluno = alunoRepository.save(aluno);
+        assertNotNull(savedAluno.getId());
+    }
 }
--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,71 @@
 package br.com.gerenciamento.repository;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import br.com.gerenciamento.model.Usuario;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Test
+    public void testFindByEmail() {
+        // Setup data for testing
+        Usuario usuario = new Usuario();
+        usuario.setEmail("test@test.com");
+        usuario.setUser("testUser");
+        usuario.setSenha("password");
+        usuarioRepository.save(usuario);
+
+        Usuario foundUsuario = usuarioRepository.findByEmail("test@test.com");
+        assertNotNull(foundUsuario);
+    }
+
+    @Test
+    public void testBuscarLogin() {
+        // Setup data for testing
+        Usuario usuario = new Usuario();
+        usuario.setUser("testUser");
+        usuario.setSenha("password");
+        usuarioRepository.save(usuario);
+
+        Usuario foundUsuario = usuarioRepository.buscarLogin("testUser", "password");
+        assertNotNull(foundUsuario);
+    }
+
+    @Test
+    public void testSave() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("novo@test.com");
+        usuario.setUser("novoUser");
+        usuario.setSenha("newPassword");
+
+        Usuario savedUsuario = usuarioRepository.save(usuario);
+        assertNotNull(savedUsuario.getId());
+    }
+
+    @Test
+    public void testDelete() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("delete@test.com");
+        usuario.setUser("deleteUser");
+        usuario.setSenha("deletePassword");
+        usuarioRepository.save(usuario);
+
+        usuarioRepository.delete(usuario);
+        Usuario foundUsuario = usuarioRepository.findByEmail("delete@test.com");
+        assertNull(foundUsuario);
+    }
 }
