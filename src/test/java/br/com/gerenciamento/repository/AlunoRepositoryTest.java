27de4808ba2f186f852d8e28bae755804fb20182
pedrowillliam <pedrowilliam014@hotmail.com>
2024-07-28 18:13:32 -0300

    testes unitarios para aluno e usuario reporsitory

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,10 +1,125 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoRepositoryTest {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Test
+    public void findByStatusAtivo() {
+        alunoRepository.deleteAll();
+
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Pedro");
+        aluno1.setTurno(Turno.NOTURNO);
+        aluno1.setCurso(Curso.ADMINISTRACAO);
+        aluno1.setStatus(Status.INATIVO);
+        aluno1.setMatricula("111111");
+        alunoRepository.save(aluno1);
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Lucas");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.ADMINISTRACAO);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setMatricula("222222");
+        alunoRepository.save(aluno2);
+
+        Aluno aluno3 = new Aluno();
+        aluno3.setNome("Moises");
+        aluno3.setTurno(Turno.NOTURNO);
+        aluno3.setCurso(Curso.ADMINISTRACAO);
+        aluno3.setStatus(Status.ATIVO);
+        aluno3.setMatricula("333333");
+        alunoRepository.save(aluno3);
+
+        List<Aluno> alunosAtivos = alunoRepository.findByStatusAtivo();
+        Assert.assertEquals(2, alunosAtivos.size());
+        Assert.assertTrue(alunosAtivos.stream().anyMatch(aluno -> aluno.getNome().equals("Lucas")));
+        Assert.assertTrue(alunosAtivos.stream().anyMatch(aluno -> aluno.getNome().equals("Moises")));
+    }
+
+    @Test
+    public void findByStatusInativo() {
+        alunoRepository.deleteAll();
+    
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Pedro");
+        aluno1.setTurno(Turno.NOTURNO);
+        aluno1.setCurso(Curso.ADMINISTRACAO);
+        aluno1.setStatus(Status.INATIVO);
+        aluno1.setMatricula("111111");
+        alunoRepository.save(aluno1);
+    
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Lucas");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.ADMINISTRACAO);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setMatricula("222222");
+        alunoRepository.save(aluno2);
+    
+        Aluno aluno3 = new Aluno();
+        aluno3.setNome("Moises");
+        aluno3.setTurno(Turno.NOTURNO);
+        aluno3.setCurso(Curso.ADMINISTRACAO);
+        aluno3.setStatus(Status.INATIVO);
+        aluno3.setMatricula("333333");
+        alunoRepository.save(aluno3);
+    
+        List<Aluno> alunosInativos = alunoRepository.findByStatusInativo();
+        Assert.assertEquals(2, alunosInativos.size());
+        Assert.assertTrue(alunosInativos.stream().anyMatch(aluno -> aluno.getNome().equals("Pedro")));
+        Assert.assertTrue(alunosInativos.stream().anyMatch(aluno -> aluno.getNome().equals("Moises")));
+    }
+    
+    @Test
+    public void salvarEEncontrarPorId() {
+        alunoRepository.deleteAll();
+
+        Aluno aluno = new Aluno();
+        aluno.setNome("Gustavo");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("444444");
+        Aluno alunoSalvo = alunoRepository.save(aluno);
+
+        Aluno alunoEncontrado = alunoRepository.findById(alunoSalvo.getId()).orElse(null);
+        Assert.assertNotNull(alunoEncontrado);
+        Assert.assertEquals("Gustavo", alunoEncontrado.getNome());
+    }
+    
+    @Test
+    public void testeSalvar() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Rafael");
+        aluno.setMatricula("6666666");
+        aluno.setCurso(Curso.ADMINISTRACAO); 
+        aluno.setStatus(Status.ATIVO); 
+        aluno.setTurno(Turno.NOTURNO); 
+
+        Aluno savedAluno = alunoRepository.save(aluno);
+        assertNotNull(savedAluno.getId());
+    }
+
 }
--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,83 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.util.Util;
+
+import static org.junit.Assert.assertNull;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Test
+    public void testeBuscarLogin() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario");
+        usuario.setEmail("usuario@example.com");
+        usuario.setSenha(Util.md5("senha"));
+        usuarioRepository.save(usuario);
+
+        Usuario foundUser = usuarioRepository.buscarLogin("usuario", Util.md5("senha"));
+        Assert.assertNotNull(foundUser);
+        Assert.assertEquals("usuario", foundUser.getUser());
+    }
+
+    @Test
+    public void testeDeletaUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("delete@example.com");
+        usuario.setUser("deleteUser");
+        usuario.setSenha("deletePassword");
+        usuarioRepository.save(usuario);
+
+        usuarioRepository.delete(usuario);
+        Usuario foundUsuario = usuarioRepository.findByEmail("delete@example.com");
+        assertNull(foundUsuario);
+    }
+        
+    @Test
+    public void testFindByEmail() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario");
+        usuario.setEmail("usuario1@example.com");
+        usuario.setSenha(Util.md5("senha"));
+        usuarioRepository.save(usuario);
+    
+        Usuario foundUser = usuarioRepository.findByEmail("usuario1@example.com");
+        Assert.assertNotNull(foundUser);
+        Assert.assertEquals("usuario", foundUser.getUser());
+    }
+    
+    @Test
+public void testUpdateUser() throws Exception {
+
+    Usuario usuario = new Usuario();
+    usuario.setUser("usuarioOriginal");
+    usuario.setEmail("usuarioOriginal@example.com");
+    usuario.setSenha(Util.md5("senhaOriginal"));
+    usuarioRepository.save(usuario);
+
+    usuario.setUser("usuarioAtualizado");
+    usuario.setSenha(Util.md5("senhaAtualizada"));
+    usuarioRepository.save(usuario);
+
+    Usuario foundUser = usuarioRepository.findByEmail("usuarioOriginal@example.com");
+    
+    Assert.assertNotNull(foundUser);
+    Assert.assertEquals("usuarioAtualizado", foundUser.getUser());
+    Assert.assertEquals(Util.md5("senhaAtualizada"), foundUser.getSenha());
+}
+
+    
 }
