7efe0649ce599ef9e57f6bbcc2742d0d2fbbb0f8
MarcosNascimento46 <mvenicio141@gmail.com>
2024-07-29 13:52:36 -0300

    Atualizacao na pasta repository

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,10 +1,60 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.model.Aluno;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class AlunoRepositoryTest {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    private Aluno aluno;
+
+    @BeforeEach
+    void setUp() {
+        aluno = new Aluno();
+        aluno.setNome("Aluno1");
+        alunoRepository.save(aluno);
+    }
+
+    @Test
+    void testFindById() {
+        Optional<Aluno> found = alunoRepository.findById(aluno.getId());
+        assertTrue(found.isPresent());
+        assertEquals(aluno.getId(), found.get().getId());
+    }
+
+    @Test
+    void testSave() {
+        Aluno novoAluno = new Aluno();
+        novoAluno.setNome("Aluno2");
+        Aluno savedAluno = alunoRepository.save(novoAluno);
+        assertNotNull(savedAluno);
+        assertEquals("Aluno2", savedAluno.getNome());
+    }
+
+    @Test
+    void testDelete() {
+        alunoRepository.delete(aluno);
+        Optional<Aluno> deletedAluno = alunoRepository.findById(aluno.getId());
+        assertFalse(deletedAluno.isPresent());
+    }
+
+    @Test
+    void testFindAll() {
+        Iterable<Aluno> alunos = alunoRepository.findAll();
+        assertNotNull(alunos);
+        assertTrue(alunos.iterator().hasNext());
+    }
 }
--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,60 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.model.Usuario;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    private Usuario usuario;
+
+    @BeforeEach
+    void setUp() {
+        usuario = new Usuario();
+        usuario.setNome("Usuario1");
+        usuarioRepository.save(usuario);
+    }
+
+    @Test
+    void testFindById() {
+        Optional<Usuario> found = usuarioRepository.findById(usuario.getId());
+        assertTrue(found.isPresent());
+        assertEquals(usuario.getId(), found.get().getId());
+    }
+
+    @Test
+    void testSave() {
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setNome("Usuario2");
+        Usuario savedUsuario = usuarioRepository.save(novoUsuario);
+        assertNotNull(savedUsuario);
+        assertEquals("Usuario2", savedUsuario.getNome());
+    }
+
+    @Test
+    void testDelete() {
+        usuarioRepository.delete(usuario);
+        Optional<Usuario> deletedUsuario = usuarioRepository.findById(usuario.getId());
+        assertFalse(deletedUsuario.isPresent());
+    }
+
+    @Test
+    void testFindAll() {
+        Iterable<Usuario> usuarios = usuarioRepository.findAll();
+        assertNotNull(usuarios);
+        assertTrue(usuarios.iterator().hasNext());
+    }
 }
