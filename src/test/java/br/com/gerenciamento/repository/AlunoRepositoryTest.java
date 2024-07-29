c022479c3d91486aefb6d3745e87d8534811beee
GustavoHenrique000 <gustavohenrique15680159@gmail.com>
2024-07-29 13:14:34 -0300

    Adicionado os testes em AlunoRepositoryTest e UsuarioRepositoryTest

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,10 +1,73 @@
 package br.com.gerenciamento.repository;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoRepositoryTest {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    // teste 1 - salvar o aluno
+    @Test
+    public void testeSalvar() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Gustavo");
+        aluno.setMatricula("123456");
+        aluno.setCurso(Curso.INFORMATICA); 
+        aluno.setStatus(Status.ATIVO); 
+        aluno.setTurno(Turno.NOTURNO); 
+
+        Aluno savedAluno = alunoRepository.save(aluno);
+        assertNotNull(savedAluno.getId());
+    }
+
+    // teste 2 - mostra status de alunos ativos
+    @Test
+    public void testeMostrarAlunosAtivos(){
+        List <Aluno> listAlunosAtivos = alunoRepository.findByStatusAtivo();
+        assertNotNull(listAlunosAtivos);
+
+        for( Aluno aluno : listAlunosAtivos){
+            assertEquals(Status.ATIVO, aluno.getStatus());
+        }
+    }
+
+    // teste 3 - mostra status de alunos inativos
+    @Test
+    public void testeMostrarAlunosInativos(){
+        List <Aluno> listAlunosInativos = alunoRepository.findByStatusInativo();
+        assertNotNull(listAlunosInativos);
+
+        for( Aluno aluno : listAlunosInativos){
+            assertEquals(Status.INATIVO, aluno.getStatus());
+        }
+    }
+
+    // teste 4 - buscar alunos pelo nome 
+    @Test
+    public void testeBuscarNomeContainingIgnoreCase() {
+        List <Aluno> listaAlunos = alunoRepository.findByNomeContainingIgnoreCase("gustavo");
+        assertNotNull(listaAlunos);
+
+        for (Aluno aluno : listaAlunos) {
+            assertTrue(aluno.getNome().toLowerCase().contains("gustavo"));
+        }
+    }
 }
--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,72 @@
 package br.com.gerenciamento.repository;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.model.Usuario;
+import jakarta.validation.ConstraintViolationException;
+
+import org.junit.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    //teste 1 - salvar usuario
+    @Test
+    public void testeSalvarUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("usuarioteste@exemplo.com");
+        usuario.setUser("usuario"); 
+        usuario.setSenha("123456789");
+        assertNotNull(this.usuarioRepository.save(usuario));
+    }
+
+    // teste 2 - buscar email que nao existe
+    @Test
+    public void testeBuscarEmailNaoEncontrado() {
+        Usuario usuarioEmail = usuarioRepository.findByEmail("usarioTeste@exemplo.com");
+        assertNull(usuarioEmail);
+    }
+
+    // teste 3 - buscar login
+    @Test
+    public void testeBuscarLogin(){
+        Usuario usuario = new Usuario();
+        usuario.setEmail("usuarioLogin@exemplo.com");
+        usuario.setUser("usuarioLogin"); 
+        usuario.setSenha("1122334455");
+        assertNotNull(this.usuarioRepository.save(usuario));
+
+        Usuario usuarioRepositorio = usuarioRepository.buscarLogin("usuarioLogin", "1122334455");
+        assertNotNull(usuarioRepositorio);
+        assertEquals("usuarioLogin", usuarioRepositorio.getUser());
+        assertEquals("1122334455", usuarioRepositorio.getSenha());
+    }
+
+    // teste 4 - mudar email do usario
+    @Test
+    public void testeMudarEmail(){
+        Usuario usuario = new Usuario();
+        usuario.setEmail("emailAntigo@exemplo.com");
+        usuario.setUser("usuarioTeste"); 
+        usuario.setSenha("123456789");
+        Usuario usuarioRep = usuarioRepository.save(usuario);
+
+        usuarioRep.setEmail("emailNovo@exemplo.com");
+        usuarioRepository.save(usuarioRep);
+
+        Usuario usuarioAtualizado = usuarioRepository.findByEmail("emailNovo@exemplo.com");
+        assertNotNull(usuarioAtualizado);
+        assertEquals("emailNovo@exemplo.com", usuarioAtualizado.getEmail());
+    }
 }
