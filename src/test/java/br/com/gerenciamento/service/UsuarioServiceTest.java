a9b6044bd634418a9bc1366ff01c458aeb10a147
Erik Queiroz <33045608+Kire87@users.noreply.github.com>
2024-07-29 02:11:32 -0300

    Add UsuarioService tests

--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,10 +1,84 @@
 package br.com.gerenciamento.service;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.exception.EmailExistsException;
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.util.Util;
+import jakarta.validation.ConstraintViolationException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioServiceTest {
+	
+	@Autowired
+	private ServiceUsuario serviceUsuario;
+	
+	@Test
+	public void salvarUsuario() throws Exception {
+		Usuario user = new Usuario();
+		user.setId(1L);
+		user.setEmail("simon@hotmail.com");
+		user.setUser("simon");
+		user.setSenha("drill");
+		this.serviceUsuario.salvarUsuario(user);
+		
+		Usuario userRetorno = this.serviceUsuario.loginUser("simon", Util.md5("drill"));
+		Assert.assertTrue(user.getEmail().equals(userRetorno.getEmail()));
+	}
+	
+	@Test
+	public void salvarEmailIgual() {
+		Usuario userA = new Usuario();
+		userA.setId(1L);
+		userA.setEmail("lelouch@hotmail.com");
+		userA.setUser("lelouch");
+		userA.setSenha("123");
+		try {
+			this.serviceUsuario.salvarUsuario(userA);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Usuario userB = new Usuario();
+		userB.setId(2L);
+		userB.setEmail("lelouch@hotmail.com");
+		userB.setUser("zero");
+		userB.setSenha("orange");
+		
+		Assert.assertThrows(EmailExistsException.class, () -> {
+            this.serviceUsuario.salvarUsuario(userB);});
+	}
+	
+	@Test
+	public void logarSemCriptografia() throws Exception {
+		Usuario user = new Usuario();
+		user.setId(1L);
+		user.setEmail("seiya@hotmail.com");
+		user.setUser("seiya");
+		user.setSenha("pegasus");
+		this.serviceUsuario.salvarUsuario(user);
+		
+		Usuario userRetorno = this.serviceUsuario.loginUser("seiya", "pegasus");
+		Assert.assertNull(userRetorno);
+	}
+	
+	@Test
+	public void senhaCaseSensitivity() throws Exception {
+		Usuario user = new Usuario();
+		user.setId(1L);
+		user.setEmail("link@hotmail.com");
+		user.setUser("link");
+		user.setSenha("MasterSword");
+		this.serviceUsuario.salvarUsuario(user);
+		
+		Usuario userRetorno = this.serviceUsuario.loginUser("seiya", Util.md5("mastersword"));
+		Assert.assertNull(userRetorno);
+	}
 }
