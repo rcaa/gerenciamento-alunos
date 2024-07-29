20dc2bf1a3b474e9dc6ec2c81714b9b1881a1930
MarcosNascimento46 <mvenicio141@gmail.com>
2024-07-29 13:54:43 -0300

    Adicao de testes em AlunoServiceTest

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -5,11 +5,16 @@ import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
 import jakarta.validation.ConstraintViolationException;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,6 +22,11 @@ public class AlunoServiceTest {
 
     @Autowired
     private ServiceAluno serviceAluno;
+
+    @Before
+    public void setUp() {
+        serviceAluno.deleteAll(); // Limpa o repositório antes de cada teste para evitar interferências.
+    }
 
     @Test
     public void getById() {
@@ -30,7 +40,7 @@ public class AlunoServiceTest {
         this.serviceAluno.save(aluno);
 
         Aluno alunoRetorno = this.serviceAluno.getById(1L);
-        Assert.assertTrue(alunoRetorno.getNome().equals("Vinicius"));
+        assertEquals("Vinicius", alunoRetorno.getNome());
     }
 
     @Test
@@ -41,7 +51,47 @@ public class AlunoServiceTest {
         aluno.setCurso(Curso.ADMINISTRACAO);
         aluno.setStatus(Status.ATIVO);
         aluno.setMatricula("123456");
-        Assert.assertThrows(ConstraintViolationException.class, () -> {
-                this.serviceAluno.save(aluno);});
+        assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    public void testFindAll() {
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Aluno1");
+        aluno1.setTurno(Turno.NOTURNO);
+        aluno1.setCurso(Curso.ADMINISTRACAO);
+        aluno1.setStatus(Status.ATIVO);
+        aluno1.setMatricula("123456");
+        
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Aluno2");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.ADMINISTRACAO);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setMatricula("654321");
+        
+        this.serviceAluno.save(aluno1);
+        this.serviceAluno.save(aluno2);
+
+        List<Aluno> alunos = serviceAluno.findAll();
+        assertNotNull(alunos);
+        assertEquals(2, alunos.size());
+    }
+
+    @Test
+    public void testDelete() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Aluno1");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        serviceAluno.delete(aluno);
+        Aluno alunoRetorno = serviceAluno.getById(aluno.getId());
+        assertNull(alunoRetorno);
     }
 }
