61b6d021a381d4406678c3823057d9f664e38c27
igordcs <igorcostamdt@gmail.com>
2024-07-29 10:56:52 -0300

    Adicionado os testes dos services

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -4,21 +4,28 @@ import br.com.gerenciamento.enums.Curso;
 import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
+import jakarta.transaction.Transactional;
 import jakarta.validation.ConstraintViolationException;
+
+import java.util.List;
+
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AlunoServiceTest {
 
     @Autowired
     private ServiceAluno serviceAluno;
 
     @Test
+    @Transactional
     public void getById() {
         Aluno aluno = new Aluno();
         aluno.setId(1L);
@@ -34,6 +41,7 @@ public class AlunoServiceTest {
     }
 
     @Test
+    @Transactional
     public void salvarSemNome() {
         Aluno aluno = new Aluno();
         aluno.setId(1L);
@@ -43,5 +51,73 @@ public class AlunoServiceTest {
         aluno.setMatricula("123456");
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
+    }
+
+    @Test
+    @Transactional
+    public void findByStatusAtivo() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("alunoo");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        List<Aluno> encontrados = this.serviceAluno.findByStatusAtivo();
+        Assert.assertTrue(encontrados.size() >= 1);
+        Assert.assertTrue(encontrados.stream().anyMatch((a) -> a.getNome().equals("alunoo")));
+    }
+
+    @Test
+    @Transactional
+    public void findByStatusInativo() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("alunoo");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.INATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        List<Aluno> encontrados = this.serviceAluno.findByStatusInativo();
+        Assert.assertTrue(encontrados.size() >= 1);
+        Assert.assertTrue(encontrados.stream().anyMatch((a) -> a.getNome().equals("alunoo")));
+    }
+
+    @Test
+    @Transactional
+    public void findByNomeContainingIgnoreCase() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("alunoo");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.INATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        List<Aluno> encontrados = this.serviceAluno.findByNomeContainingIgnoreCase("AlUnO");
+        Assert.assertTrue(encontrados.size() >= 1);
+        Assert.assertTrue(encontrados.stream().anyMatch((a) -> a.getNome().equals("alunoo")));
+    }
+
+    @Test
+    @Transactional
+    public void findAll() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("alunoo");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.INATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        List<Aluno> encontrados = this.serviceAluno.findAll();
+        Assert.assertTrue(encontrados.size() >= 1);
+        Assert.assertTrue(encontrados.stream().anyMatch((a) -> a.getNome().equals("alunoo")));
     }
 }
--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,10 +1,77 @@
 package br.com.gerenciamento.service;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.exception.EmailExistsException;
+import br.com.gerenciamento.model.Usuario;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolationException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioServiceTest {
+    
+    @Autowired
+    private ServiceUsuario serviceUsuario;
+
+    @Test
+    @Transactional
+    public void salvarUsuario()  {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("usuario");
+        usuario.setSenha("123456");
+        usuario.setEmail("nome@hotmail.com");
+        assertDoesNotThrow(() -> this.serviceUsuario.salvarUsuario(usuario));        
+    }
+
+    @Test
+    @Transactional
+    public void salvarEmailMalFormatado() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("usuario");
+        usuario.setSenha("123456");
+        usuario.setEmail("igor.com");
+        Assert.assertThrows(ConstraintViolationException.class, () -> this.serviceUsuario.salvarUsuario(usuario));
+    }
+
+    @Test
+    @Transactional
+    public void salvarEmailJaCadastrado() throws Exception{
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("usuario");
+        usuario.setSenha("123456");
+        usuario.setEmail("EmailTeste@hotmail.com");
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2L);
+        usuario2.setUser("usuario2");
+        usuario2.setSenha("12345678");
+        usuario2.setEmail("EmailTeste@hotmail.com");
+        Assert.assertThrows(EmailExistsException.class, () -> this.serviceUsuario.salvarUsuario(usuario2));
+    }
+
+    @Test
+    @Transactional
+    public void fazerLogin() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("usuario");
+        usuario.setSenha("123456");
+        usuario.setEmail("usuario@hotmail.com");
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        Usuario login = this.serviceUsuario.loginUser(usuario.getUser(), usuario.getSenha());
+        Assert.assertEquals("usuario", login.getUser());
+    }
 }
