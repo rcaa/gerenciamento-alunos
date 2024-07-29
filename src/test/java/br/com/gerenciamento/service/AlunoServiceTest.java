256eebee1fd748a3cddec502cd705289764b5d7c
Laysa Protásio <117881941+laysaprotasio@users.noreply.github.com>
2024-07-29 02:00:03 -0300

    Testes do service de usuários e alunos

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -5,6 +5,9 @@ import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
 import jakarta.validation.ConstraintViolationException;
+
+import java.util.NoSuchElementException;
+
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +45,61 @@ public class AlunoServiceTest {
         aluno.setStatus(Status.ATIVO);
         aluno.setMatricula("123456");
         Assert.assertThrows(ConstraintViolationException.class, () -> {
-                this.serviceAluno.save(aluno);});
+            this.serviceAluno.save(aluno);
+        });
     }
+
+    @Test
+    public void getByIdNotFound() {
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+            this.serviceAluno.getById(99L);
+        });
+    }
+
+    @Test
+    public void deleteAluno() {
+        Aluno aluno = new Aluno();
+        aluno.setId(3L);
+        aluno.setNome("Carlos");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("789012");
+        this.serviceAluno.save(aluno);
+
+        this.serviceAluno.deleteById(aluno.getId());
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+            this.serviceAluno.getById(aluno.getId());
+        });
+    }
+
+    @Test
+    public void salvarSemMatricula() {
+        Aluno aluno = new Aluno();
+        aluno.setId(2L);
+        aluno.setNome("Ana");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.CONTABILIDADE);
+        aluno.setStatus(Status.ATIVO);
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    public void salvarSemStatus() {
+        Aluno aluno = new Aluno();
+        aluno.setId(3L);
+        aluno.setNome("Carlos");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setMatricula("789012");
+        
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    
 }
--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,10 +1,74 @@
 package br.com.gerenciamento.service;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.service.ServiceUsuario;
+import ch.qos.logback.classic.pattern.Util;
+import jakarta.validation.ConstraintViolationException;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.exception.CriptoExistsException;
+import br.com.gerenciamento.exception.EmailExistsException;
+import br.com.gerenciamento.model.Usuario;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioServiceTest {
+
+    @Autowired
+    private ServiceUsuario serviceUsuario;
+
+    @Test
+    public void salvarUsuario() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("Teste1");
+        usuario.setEmail("test@example.com");
+        usuario.setSenha("senha");
+        serviceUsuario.salvarUsuario(usuario);
+
+        Assert.assertTrue(usuario.getUser().equals("Teste1"));
+
+    }
+
+    @Test
+    public void salvarUsuarioComEmailDuplicado() throws Exception {
+        Usuario usuario1 = new Usuario();
+        usuario1.setUser("TesteDuplicado");
+        usuario1.setEmail("duplicado@example.com");
+        usuario1.setSenha("senha456");
+
+        serviceUsuario.salvarUsuario(usuario1);
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setUser("TesteDuplicado2");
+        usuario2.setEmail("duplicado@example.com");
+        usuario2.setSenha("senha789");
+
+        Assert.assertThrows(EmailExistsException.class, () -> {
+            serviceUsuario.salvarUsuario(usuario2);
+        });
+
+    }
+
+    @Test
+    public void loginUserComCredenciaisInvalidas() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("UsuarioLogin");
+        usuario.setEmail("login@example.com");
+        usuario.setSenha("senha");
+        serviceUsuario.salvarUsuario(usuario);
+
+        Usuario usuarioLogado = serviceUsuario.loginUser("UsuarioLogin", "senhaErrada");
+        assertNull(usuarioLogado);
+    }
+
 }
