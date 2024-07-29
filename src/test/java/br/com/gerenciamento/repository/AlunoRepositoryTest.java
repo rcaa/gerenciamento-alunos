a30f59dd22fb5ea1feb785319e34fb8941640af5
igordcs <igorcostamdt@gmail.com>
2024-07-29 10:57:45 -0300

    adicionado os testes do repository

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,10 +1,85 @@
 package br.com.gerenciamento.repository;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import jakarta.transaction.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoRepositoryTest {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+    
+    private Aluno aluno = new Aluno();
+    private Aluno aluno2 = new Aluno();
+
+    @Before
+    public void setUp() {
+        aluno.setId(1L);
+        aluno.setNome("alunoo");
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.BIOMEDICINA);
+        aluno.setMatricula("123456");
+
+        aluno2.setId(2L);
+        aluno2.setNome("inativo");
+        aluno2.setStatus(Status.INATIVO);
+        aluno2.setTurno(Turno.MATUTINO);
+        aluno2.setCurso(Curso.BIOMEDICINA);
+        aluno2.setMatricula("123456");
+
+        this.alunoRepository.save(aluno);
+        this.alunoRepository.save(aluno2);
+    }
+
+    @After
+    public void reset() {
+        this.alunoRepository.deleteAll();
+    }
+
+    @Test
+    public void findAll() {
+        List<Aluno> alunosEncontrados = this.alunoRepository.findAll();
+        Assert.assertEquals(2, alunosEncontrados.size());
+        Assert.assertTrue(alunosEncontrados.stream().anyMatch(a -> a.getNome().equals(aluno.getNome())));
+        Assert.assertTrue(alunosEncontrados.stream().anyMatch(a -> a.getNome().equals(aluno2.getNome())));
+    }
+
+    @Test
+    public void findByStatusAtivo() {
+        List<Aluno> alunosEncontrados = this.alunoRepository.findByStatusAtivo();
+        Assert.assertEquals(1, alunosEncontrados.size());
+        Assert.assertTrue(alunosEncontrados.stream().anyMatch(a -> a.getNome().equals(aluno.getNome())));
+    }
+
+    @Test
+    public void findByStatusInativo() {
+        List<Aluno> alunosEncontrados = this.alunoRepository.findByStatusInativo();
+        Assert.assertEquals(1, alunosEncontrados.size());
+        Assert.assertTrue(alunosEncontrados.stream().anyMatch(a -> a.getNome().equals(aluno2.getNome())));
+    }
+
+    @Test
+    public void findByNomeContainingIgnoreCase() {
+        List<Aluno> alunosEncontrados = this.alunoRepository.findByNomeContainingIgnoreCase("aLuNo");
+        Assert.assertEquals(1, alunosEncontrados.size());
+        Assert.assertTrue(alunosEncontrados.stream().anyMatch(a -> a.getNome().equals(aluno.getNome())));
+    }
 }
--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,65 @@
 package br.com.gerenciamento.repository;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.model.Usuario;
+import jakarta.transaction.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    
+    private Usuario usuario = new Usuario();
+    
+    @Before
+    public void setUp() {
+        usuario.setId(2L);
+        usuario.setUser("usuario");
+        usuario.setSenha("123456");
+        usuario.setEmail("teste@hotmail.com");
+        this.usuarioRepository.save(usuario);
+    }
+    
+    @After
+    public void reset() {
+        this.usuarioRepository.deleteAll();
+    }
+
+    @Test
+    public void findAll() {
+        List<Usuario> usuarioEncontrado = this.usuarioRepository.findAll();
+        Assert.assertTrue(usuarioEncontrado.stream().anyMatch(u -> u.getUser().equals(usuario.getUser()))); 
+    }
+
+    @Test
+    public void findById() {
+        Usuario usuarioEncontrado = this.usuarioRepository.findById(2L).orElse(null);
+        Assert.assertNotNull(usuarioEncontrado); 
+        Assert.assertEquals(usuario.getUser(), usuarioEncontrado.getUser()); 
+    }
+
+    @Test
+    public void findbyEmail() {
+        Usuario usuarioEncontrado = this.usuarioRepository.findByEmail("teste@hotmail.com");
+        Assert.assertEquals(usuario.getUser(), usuarioEncontrado.getUser()); 
+    }
+
+    @Test
+    public void buscarLogin() {
+        Usuario usuarioEncontrado = this.usuarioRepository.buscarLogin("usuario", "123456");
+        Assert.assertEquals(usuario.getUser(), usuarioEncontrado.getUser()); 
+    }
 }
