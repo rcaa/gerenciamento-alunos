f06bbf76bd982d777d09ebc769c0dd672a1e9922
Erik Queiroz <33045608+Kire87@users.noreply.github.com>
2024-07-29 02:10:01 -0300

    Add AlunoService tests

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -5,6 +5,9 @@ import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
 import jakarta.validation.ConstraintViolationException;
+
+import java.util.List;
+
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +46,75 @@ public class AlunoServiceTest {
         aluno.setMatricula("123456");
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
+    }
+    
+    @Test
+    public void findByStatusInativo() {
+    	Aluno aluno = new Aluno();
+    	aluno.setId(1L);
+    	aluno.setNome("Vinicius");
+    	aluno.setTurno(Turno.NOTURNO);
+    	aluno.setCurso(Curso.ADMINISTRACAO);
+    	aluno.setStatus(Status.INATIVO);
+    	aluno.setMatricula("123456");
+    	this.serviceAluno.save(aluno);
+    	
+    	Aluno alunoRetorno = this.serviceAluno.findByStatusInativo().get(0);
+    	Assert.assertTrue(alunoRetorno.getNome().equals("Vinicius"));
+    }
+    
+    @Test
+    public void deleteById() {
+    	Aluno alunoA = new Aluno();
+    	alunoA.setId(1L);
+    	alunoA.setNome("Vinicius");
+    	alunoA.setTurno(Turno.NOTURNO);
+    	alunoA.setCurso(Curso.ADMINISTRACAO);
+    	alunoA.setStatus(Status.ATIVO);
+    	alunoA.setMatricula("123456");
+    	this.serviceAluno.save(alunoA);
+    	
+    	Aluno alunoB = new Aluno();
+    	alunoB.setId(2L);
+    	alunoB.setNome("Lelouch");
+    	alunoB.setTurno(Turno.NOTURNO);
+    	alunoB.setCurso(Curso.ADMINISTRACAO);
+    	alunoB.setStatus(Status.ATIVO);
+    	alunoB.setMatricula("654321");
+    	this.serviceAluno.save(alunoB);
+    	
+    	this.serviceAluno.deleteById(1L);
+    	
+    	List<Aluno> alunos = this.serviceAluno.findAll();
+    	int tamanhoLista = alunos.size();
+    	
+    	Assert.assertTrue(tamanhoLista == 1);
+    }
+    
+    @Test
+    public void salvarSemId() {
+    	Aluno aluno = new Aluno();
+    	aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+                this.serviceAluno.save(aluno);});
+    }
+    
+    @Test
+    public void findByNomeContainingIgnoreCase() {
+    	Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        List<Aluno> alunoRetorno = this.serviceAluno.findByNomeContainingIgnoreCase("vinicius");
+        Assert.assertTrue(alunoRetorno.get(0).getNome().equals("Vinicius"));
     }
 }
