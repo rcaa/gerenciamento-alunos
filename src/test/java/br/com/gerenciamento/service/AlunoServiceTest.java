444f8f5bd9f8148bce9ee5d542732bc65ec4a88e
Matheusxr77 <suehtamxr77@gmail.com>
2024-07-28 21:51:09 -0300

    add testes em AlunoServiceTest

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -4,9 +4,17 @@ import br.com.gerenciamento.enums.Curso;
 import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
+
+import jakarta.transaction.Transactional;
 import jakarta.validation.ConstraintViolationException;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.*;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,29 +27,140 @@ public class AlunoServiceTest {
     private ServiceAluno serviceAluno;
 
     @Test
+    @Transactional
     public void getById() {
         Aluno aluno = new Aluno();
+
         aluno.setId(1L);
         aluno.setNome("Vinicius");
         aluno.setTurno(Turno.NOTURNO);
         aluno.setCurso(Curso.ADMINISTRACAO);
         aluno.setStatus(Status.ATIVO);
         aluno.setMatricula("123456");
+
         this.serviceAluno.save(aluno);
 
         Aluno alunoRetorno = this.serviceAluno.getById(1L);
+
         Assert.assertTrue(alunoRetorno.getNome().equals("Vinicius"));
     }
 
     @Test
+    @Transactional
     public void salvarSemNome() {
         Aluno aluno = new Aluno();
+
         aluno.setId(1L);
         aluno.setTurno(Turno.NOTURNO);
         aluno.setCurso(Curso.ADMINISTRACAO);
         aluno.setStatus(Status.ATIVO);
         aluno.setMatricula("123456");
+
         Assert.assertThrows(ConstraintViolationException.class, () -> {
-                this.serviceAluno.save(aluno);});
+                this.serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    @Transactional
+    public void salvarSemId() {
+        Aluno aluno = new Aluno();
+
+        aluno.setNome("Aparecida");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.ENFERMAGEM);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("789");
+
+        this.serviceAluno.save(aluno);
+
+        if (aluno.getId() == null) {
+            Assert.fail("Aluno salvo sem id");
+        }
+    }
+
+    @Test
+    @Transactional
+    public void findAll() {
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno();
+
+        aluno1.setNome("Matheus");
+        aluno1.setTurno(Turno.MATUTINO);
+        aluno1.setCurso(Curso.ADMINISTRACAO);
+        aluno1.setStatus(Status.ATIVO);
+        aluno1.setMatricula("123");
+
+        aluno2.setNome("Marcos");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.INFORMATICA);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setMatricula("234");
+
+        this.serviceAluno.save(aluno1);
+        this.serviceAluno.save(aluno2);
+
+        List<Aluno> alunos = this.serviceAluno.findAll();
+
+        Assert.assertEquals(2, alunos.size());
+
+        if (alunos.get(0).getId() == aluno1.getId()) {
+            Assert.assertEquals(alunos.get(1).getId(), aluno2.getId());
+        } else if (alunos.get(0).getId() == aluno2.getId()) {
+            Assert.assertEquals(alunos.get(2).getId(), aluno1.getId());
+        } else {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    @Transactional
+    public void deleteById() {
+        Aluno aluno = new Aluno();
+
+        aluno.setId(1L);
+        aluno.setNome("Joel da Silva");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.BIOMEDICINA);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("345");
+
+        this.serviceAluno.save(aluno);
+
+        if (aluno.getId() == null) {
+            Assert.fail("Aluno salvo sem id");
+        } else {
+            this.serviceAluno.deleteById(1L);
+        }
+    }
+
+    @Test
+    @Transactional
+    public void findByStatusInativo() {
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno();
+
+        aluno1.setNome("João Grilo");
+        aluno1.setTurno(Turno.NOTURNO);
+        aluno1.setCurso(Curso.DIREITO);
+        aluno1.setStatus(Status.INATIVO);
+        aluno1.setMatricula("321");
+
+        aluno2.setNome("Chicó");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.CONTABILIDADE);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setMatricula("456");
+
+        this.serviceAluno.save(aluno1);
+        this.serviceAluno.save(aluno2);
+
+        List<Aluno> alunosInativos = this.serviceAluno.findByStatusInativo();
+
+        if (alunosInativos.isEmpty()) {
+            Assert.fail("Lista de alunos inativos está vazia");
+        } else {
+            assertTrue(alunosInativos.stream().allMatch(a -> Status.INATIVO.equals(a.getStatus())));
+        }
     }
 }
