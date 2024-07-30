cf6d939dc9af65ccc9c0587ad19c1e9b9cd801c1
Moonin371 <luanabrito285@gmail.com>
2024-07-30 10:33:34 -0300

    Testes de Integração

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,117 @@
 package br.com.gerenciamento.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.service.ServiceAluno;
+import jakarta.transaction.Transactional;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 public class AlunoControllerTest {
+
+    @Autowired
+    private AlunoController alunoController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void inserirAluno() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+
+        this.alunoController.insertAlunos();
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/InsertAlunos", aluno, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+    }
+
+    @Test
+    public void listarAlunos() {
+        ServiceAluno serviceAluno = new ServiceAluno();
+
+        Aluno aluno1 = new Aluno();
+        aluno1.setId(1L);
+        aluno1.setNome("Vinicius");
+        aluno1.setTurno(Turno.NOTURNO);
+        aluno1.setCurso(Curso.ADMINISTRACAO);
+        aluno1.setStatus(Status.ATIVO);
+        aluno1.setMatricula("123456");
+        serviceAluno.save(aluno1);
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setId(1L);
+        aluno2.setNome("João da Silva");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.ADMINISTRACAO);
+        aluno2.setStatus(Status.INATIVO);
+        aluno2.setMatricula("294830");
+        serviceAluno.save(aluno2);
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/alunos-adicionados", String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(response.getBody().contains("Vinicius"));
+        assertTrue(response.getBody().contains("João da Silva"));
+    }
+
+    @Test
+    public void editarAluno() {
+        ServiceAluno serviceAluno = new ServiceAluno();
+
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        serviceAluno.save(aluno);
+
+        aluno.setNome("Aluno Editado");
+        restTemplate.postForEntity("/editar", aluno, String.class);
+
+        Aluno alunoAtualizado = serviceAluno.getById(aluno.getId());
+        assertEquals("Aluno Editado", alunoAtualizado.getNome());
+    }
+
+    @Test
+    public void excluirAluno() {
+        ServiceAluno serviceAluno = new ServiceAluno();
+
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        serviceAluno.save(aluno);
+        restTemplate.getForEntity("/remover/" + aluno.getId(), String.class);
+        assertFalse(serviceAluno.findAll().contains(aluno));
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,97 @@
 package br.com.gerenciamento.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.service.ServiceUsuario;
+import jakarta.transaction.Transactional;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 public class UsuarioControllerTest {
+    @Autowired
+    private UsuarioController usuarioController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ServiceUsuario serviceUsuario;
+
+    @Test
+    public void login() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setEmail("teste@teste.com");
+        usuario.setSenha("1234");
+        usuario.setUser("Júlia");
+
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/login", usuario, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(response.getBody().contains("home/index"));
+    }
+
+    @Test
+    public void loginFalho() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setEmail("teste@teste.com");
+        usuario.setSenha("1234");
+        usuario.setUser("Júlia");
+
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        usuario.setSenha("senhaErrada");
+        ResponseEntity<String> response = restTemplate.postForEntity("/login", usuario, String.class);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertTrue(response.getBody().contains("Usuario não encontrado. Tente novamente"));
+    }
+
+    @Test
+    public void cadastro() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setEmail("teste@teste.com");
+        usuario.setSenha("1234");
+        usuario.setUser("Júlia");
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/salvarUsuario", usuario, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(response.getHeaders().getLocation().toString().contains("/"));
+    }
+
+    @Test
+    public void logout() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setEmail("teste@teste.com");
+        usuario.setSenha("1234");
+        usuario.setUser("Júlia");
+
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        ResponseEntity<String> loginResponse = restTemplate.postForEntity("/login", usuario, String.class);
+        assertEquals(HttpStatus.OK, loginResponse.getStatusCode());
+
+        ResponseEntity<String> logoutResponse = restTemplate.postForEntity("/logout", null, String.class);
+        assertEquals(HttpStatus.OK, logoutResponse.getStatusCode());
+        assertTrue(logoutResponse.getBody().contains("login/login"));
+
+    }
 }
