3f04e7e3a8595fb0f5a38206b409784c4e219923
Carlos Eduardo B. S. Araujo <cadu29bahia@gmail.com>
2024-07-31 01:12:51 -0300

    fix: Ajuste para que os testes sejam executados sequencialmente, e limpando a sujeira entre os testes, para evitar erros equivocados nos testes

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -9,12 +9,14 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AlunoServiceTest {
 
     @Autowired
