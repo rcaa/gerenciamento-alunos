c3a489181e76535ec9f297bf606f1d06fc918596
Carlos Eduardo B. S. Araujo <cadu29bahia@gmail.com>
2024-07-31 00:24:08 -0300

    feat: Testes dos serviços de Aluno

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoServiceTest {
@@ -44,4 +46,104 @@ public class AlunoServiceTest {
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
     }
+
+    @Test
+    public void findByStatusAtivoAndCount() {
+        //Adiciona 1 aluno ativo
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        //Adiciona 1 aluno inativo
+        Aluno aluno2 = new Aluno();
+        aluno2.setId(2L);
+        aluno2.setNome("Rodrigo");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.ADMINISTRACAO);
+        aluno2.setStatus(Status.INATIVO);
+        aluno2.setMatricula("213456");
+        this.serviceAluno.save(aluno2);
+
+        //busca pelos alunos ativos
+        List<Aluno> alunoList = serviceAluno.findByStatusAtivo();
+
+        //Verifica se a resposta do serviço retornou uma lista contendo o aluno ativo
+        Assert.assertTrue(alunoList.stream().anyMatch(alunoAtivo -> alunoAtivo.getId().equals(aluno.getId())));
+
+        //Verifica se a resposta retornou uma lista unitária
+        Assert.assertEquals(1, alunoList.size());
+    }
+
+    @Test
+    public void buscarPorAlunoPorNome() {
+        //Salva um aluno com nome composto por exemplo
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius da Mata Rocha");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        //Salva um aluno com nome composto por exemplo
+        Aluno aluno2 = new Aluno();
+        aluno2.setId(2L);
+        aluno2.setNome("Carlos Vinicius Rodrigues");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.ADMINISTRACAO);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setMatricula("123456");
+        this.serviceAluno.save(aluno2);
+
+        List<Aluno> alunoVini = serviceAluno.findByNomeContainingIgnoreCase("vini");
+
+        Assert.assertEquals(2, alunoVini.size());
+    }
+
+    @Test
+    public void buscarTodosAlunos() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius da Mata Rocha");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setId(2L);
+        aluno2.setNome("Carlos Vinicius Rodrigues");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.INFORMATICA);
+        aluno2.setStatus(Status.INATIVO);
+        aluno2.setMatricula("423658");
+        this.serviceAluno.save(aluno2);
+
+        List<Aluno> alunos = serviceAluno.findAll();
+
+        Assert.assertEquals(2, alunos.size());
+    }
+
+    @Test
+    public void salvarAlunoComNome3caracteres() {
+
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vin");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);});
+    }
+
 }
