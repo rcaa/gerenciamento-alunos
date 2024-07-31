f78d4e186594afdb7a5d25723367fd10b58e7e0a
Carlos Eduardo B. S. Araujo <cadu29bahia@gmail.com>
2024-07-31 02:57:15 -0300

    feat: Testes de integração para o Controller de Aluno

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,87 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.service.ServiceAluno;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc //Faz o trabalho do Before para configurar o MockMvc
 public class AlunoControllerTest {
+
+    @Autowired
+    private ServiceAluno serviceAluno;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void removerAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/remover/" + aluno.getId()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/alunos-adicionados"));
+    }
+
+    @Test
+    public void cadastrarAluno()throws Exception{
+
+        this.mockMvc.perform(post("/InsertAlunos")
+                        .formField("nome", "Teste")
+                        .formField("curso", "INFORMATICA")
+                        .formField("matricula", "2024")
+                        .formField("turno", "NOTURNO")
+                        .formField("status", "ATIVO"))
+                .andExpect(view().name("redirect:/alunos-adicionados"));
+    }
+
+    @Test
+    public void listarAlunos() throws Exception
+    {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos-adicionados"))
+                .andExpect(view().name("Aluno/listAlunos"))
+                .andExpect(model().attribute("alunosList", hasItem(hasProperty("nome", containsString("Vinicius")))));
+    }
+
+    @Test
+    public void listaAlunosAtivos() throws Exception
+    {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        this.serviceAluno.save(aluno);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/alunos-ativos")).andExpect(view().name("Aluno/alunos-ativos"))
+                .andExpect(model().attribute("alunosAtivos", hasItem(hasProperty("nome", containsString("Vinicius")))));
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -4,32 +4,22 @@ import br.com.gerenciamento.model.Usuario;
 import br.com.gerenciamento.service.ServiceUsuario;
 import br.com.gerenciamento.util.Util;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.ConstraintViolationException;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.security.NoSuchAlgorithmException;
-
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
