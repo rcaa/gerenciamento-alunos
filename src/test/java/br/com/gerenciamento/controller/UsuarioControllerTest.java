f93ed8559a6d4a4969f500bda1c3bedc6bdf8666
Douglas Morais <douglas.leitemorais@gmail.com>
2024-09-08 21:36:09 -0300

    testes UsuarioControllerTest

--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,14 +1,20 @@
 package br.com.gerenciamento.controller;
 
-import org.aspectj.lang.annotation.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,8 +22,53 @@ import org.springframework.web.context.WebApplicationContext;
 public class UsuarioControllerTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
     private MockMvc mockMvc;
 
+    @Test
+    @Transactional
+    public void login() throws Exception {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("login/login"))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("usuario"));
+    }
 
+    @Test
+    @Transactional
+    public void logout() throws Exception
+    {
+        this.mockMvc.perform(post("/logout"))
+                .andExpect(view().name("login/login"));
+    }
+
+    @Test
+    @Transactional
+    public void index() throws Exception {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/index"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("home/index"))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("aluno"));
+    }
+
+    @Test
+    @Transactional
+    public void cadastrar() throws Exception {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/cadastro"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("login/cadastro"))
+            .andExpect(MockMvcResultMatchers.model().attributeExists("usuario"));
+    }
+
+    @Test
+    @Transactional
+    public void testSalvarUsuario() throws Exception {
+        mockMvc.perform(post("/salvarUsuario")
+                .param("nome", "Test User")
+                .param("senha", "12345"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
 }
