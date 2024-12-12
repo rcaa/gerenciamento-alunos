3011c2deba58670648405be46e4739f451be21ab
Victor Saraiva <victor_alex2005@hotmail.com>
2024-12-12 12:42:07 -0300

    4 testes unitarios do AlunoServiceTest,
    - teste para salvar sem matricula
    - teste para salvar um nome muito longo e dar erro
    - teste para salvar um nome curto e dar erro
    - teste para salvar uma matricula muito curta e dar erro

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -11,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.NoSuchElementException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class AlunoServiceTest {
 
     @Autowired
@@ -42,6 +45,67 @@ public class AlunoServiceTest {
         aluno.setStatus(Status.ATIVO);
         aluno.setMatricula("123456");
         Assert.assertThrows(ConstraintViolationException.class, () -> {
-                this.serviceAluno.save(aluno);});
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    // #1 teste para salvar sem matricula
+    @Test
+    public void salvarSemMatricula() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Fulano");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula(null); // Definindo matrícula como null
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    // #2 teste para salvar um nome muito longo e dar erro
+    @Test
+    public void salvarNomeGrande() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Nome muito grande que talvez alguem tenha");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.BIOMEDICINA); // colocando um curso que não existe
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    // #3 teste para salvar um nome curto e dar erro
+    @Test
+    public void salvarNomeCurto() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Nome");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.BIOMEDICINA); // colocando um curso que não existe
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    // #4 teste para salvar uma matricula muito curta e dar erro
+    @Test
+    public void salvarMatriculaCurta() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Fulano");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.BIOMEDICINA); // colocando um curso que não existe
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("12");
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
     }
 }
