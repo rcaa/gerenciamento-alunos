69669b2a45661c164d7dbfa4369ea573b6c01375
RafaelaFoerster <rafafoerst@gmail.com>
2024-12-12 01:29:22 -0300

    Implementando testes unitários para AlunoServiceTest

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -42,6 +42,66 @@ public class AlunoServiceTest {
         aluno.setStatus(Status.ATIVO);
         aluno.setMatricula("123456");
         Assert.assertThrows(ConstraintViolationException.class, () -> {
-                this.serviceAluno.save(aluno);});
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    public void salvarComNomeMuitoCurto() {
+        Aluno aluno = new Aluno();
+        aluno.setId(3L);
+        aluno.setNome("Ana"); // Nome com menos de 5 caracteres
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("789012");
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    public void salvarAlunoComMatriculaInvalida() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Ana Paula");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("12"); // Matrícula inválida (menos de 3 caracteres)
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    public void salvarSemMatricula() {
+        Aluno aluno = new Aluno();
+        aluno.setId(5L);
+        aluno.setNome("Mariana Oliveira");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ENFERMAGEM);
+        aluno.setStatus(Status.ATIVO);
+        // Não define a matrícula
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    public void deleteAlunoById() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Carlos");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("141516");
+        this.serviceAluno.save(aluno);
+
+        this.serviceAluno.deleteById(aluno.getId());
+
+        Assert.assertThrows(RuntimeException.class, () -> {
+            this.serviceAluno.getById(aluno.getId());
+        });
     }
 }
