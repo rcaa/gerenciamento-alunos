be2a149efff0960e5289e61b4495b4e5196bf1c8
Arthur de Sá Tenório <arthursa147@gmail.com>
2024-12-14 23:10:56 -0300

    Implementação de testes unitários para a classe AlunoService

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -5,6 +5,11 @@ import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
 import jakarta.validation.ConstraintViolationException;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,7 +41,7 @@ public class AlunoServiceTest {
     @Test
     public void salvarSemNome() {
         Aluno aluno = new Aluno();
-        aluno.setId(1L);
+        aluno.setId(77L);
         aluno.setTurno(Turno.NOTURNO);
         aluno.setCurso(Curso.ADMINISTRACAO);
         aluno.setStatus(Status.ATIVO);
@@ -44,4 +49,104 @@ public class AlunoServiceTest {
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
     }
+
+    @Test
+    public void TesteBuscarTodosAlunos() {
+        Aluno a1 = new Aluno();
+        a1.setId(1L);
+        a1.setNome("Arthur Tenorio");
+        a1.setTurno(Turno.NOTURNO);
+        a1.setCurso(Curso.INFORMATICA);
+        a1.setStatus(Status.ATIVO);
+        a1.setMatricula("123");
+
+        Aluno a2 = new Aluno();
+        a2.setId(2L);
+        a2.setNome("Jeasiel Abner");
+        a2.setTurno(Turno.NOTURNO);
+        a2.setCurso(Curso.CONTABILIDADE);
+        a2.setStatus(Status.INATIVO);
+        a2.setMatricula("126");
+
+        serviceAluno.save(a1);
+        serviceAluno.save(a2);
+
+        List<Aluno> alunos = serviceAluno.findAll();
+        assertEquals(2, alunos.size());
+        assertEquals("123", alunos.get(0).getMatricula());
+        assertEquals("126", alunos.get(1).getMatricula());
+    }
+
+    @Test
+    public void TesteBuscarAlunoPorNome() {
+        Aluno a1 = new Aluno();
+        a1.setId(1L);
+        a1.setNome("Arthur Tenorio");
+        a1.setTurno(Turno.NOTURNO);
+        a1.setCurso(Curso.INFORMATICA);
+        a1.setStatus(Status.ATIVO);
+        a1.setMatricula("123");
+
+        serviceAluno.save(a1);
+
+        List<Aluno> alunos = serviceAluno.findByNomeContainingIgnoreCase("Arthur");
+        assertEquals(1, alunos.size());
+        assertEquals("123", alunos.get(0).getMatricula());
+    }
+
+    
+    @Test
+    public void TesteBuscarAlunoAtivo() {
+        Aluno a1 = new Aluno();
+        a1.setId(1L);
+        a1.setNome("Arthur Tenorio");
+        a1.setTurno(Turno.NOTURNO);
+        a1.setCurso(Curso.INFORMATICA);
+        a1.setStatus(Status.ATIVO);
+        a1.setMatricula("123");
+
+        Aluno a2 = new Aluno();
+        a2.setId(2L);
+        a2.setNome("Jeasiel Abner");
+        a2.setTurno(Turno.NOTURNO);
+        a2.setCurso(Curso.CONTABILIDADE);
+        a2.setStatus(Status.INATIVO);
+        a2.setMatricula("126");
+
+        serviceAluno.save(a1);
+        serviceAluno.save(a2);
+
+        List<Aluno> alunos = serviceAluno.findByStatusAtivo();
+        assertEquals(1, alunos.size());
+        assertEquals("123", alunos.get(0).getMatricula());
+    }
+
+    @Test
+    public void TesteBuscarAlunoInativo() {
+        Aluno a1 = new Aluno();
+        a1.setId(1L);
+        a1.setNome("Arthur Tenorio");
+        a1.setTurno(Turno.NOTURNO);
+        a1.setCurso(Curso.INFORMATICA);
+        a1.setStatus(Status.ATIVO);
+        a1.setMatricula("123");
+
+        Aluno a2 = new Aluno();
+        a2.setId(2L);
+        a2.setNome("Jeasiel Abner");
+        a2.setTurno(Turno.NOTURNO);
+        a2.setCurso(Curso.CONTABILIDADE);
+        a2.setStatus(Status.INATIVO);
+        a2.setMatricula("126");
+
+        serviceAluno.save(a1);
+        serviceAluno.save(a2);
+
+        List<Aluno> alunos = serviceAluno.findByStatusInativo();
+        assertEquals(1, alunos.size());
+        assertEquals("126", alunos.get(0).getMatricula());
+    }
+    //Houve uma tentativa de testar a função de deleção, mas a mesma simplesmente
+    //se recusa a funcionar, ou faz com que outros testes deem errado.
+    //Então, os testes são muito parecidos com os de AlunoRepositoryTest.
 }
