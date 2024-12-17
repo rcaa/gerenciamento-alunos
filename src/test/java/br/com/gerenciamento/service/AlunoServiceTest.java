c894e7cd1008ca1218205e4f1a0fd5de07b0615b
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2024-12-16 22:25:54 -0300

    teste unitario em AlunoServiceTest

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -1,16 +1,17 @@
 package br.com.gerenciamento.service;
 
-import br.com.gerenciamento.enums.Curso;
-import br.com.gerenciamento.enums.Status;
-import br.com.gerenciamento.enums.Turno;
-import br.com.gerenciamento.model.Aluno;
-import jakarta.validation.ConstraintViolationException;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import jakarta.validation.ConstraintViolationException;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoServiceTest {
@@ -44,4 +45,38 @@ public class AlunoServiceTest {
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
     }
+
+    @Test
+    public void salvarComNomeValido() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Jefferson");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.ENFERMAGEM);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("654321");
+
+        this.serviceAluno.save(aluno);
+
+        Aluno alunoSalvo = this.serviceAluno.getById(1L);
+        Assert.assertNotNull(alunoSalvo); 
+        Assert.assertEquals("Jefferson", alunoSalvo.getNome());
+        Assert.assertEquals("654321", alunoSalvo.getMatricula());
+    }
+
+    @Test
+    public void salvarComMatriculaInvalida() {
+    Aluno aluno = new Aluno();
+    aluno.setId(1L);
+    aluno.setNome("Carlos");
+    aluno.setTurno(Turno.NOTURNO);
+    aluno.setCurso(Curso.DIREITO);
+    aluno.setStatus(Status.ATIVO);
+    aluno.setMatricula("12"); 
+    
+    Assert.assertThrows(ConstraintViolationException.class, () -> {
+        this.serviceAluno.save(aluno);
+    });
+}
+
 }
