d1f46696b12391fd48f0888bf398158dc9e3d4cf
luisfelipe03 <llipeandrade34@gmail.com>
2024-12-17 16:43:08 -0300

    :test_tube: test: testes unitarios do AlunoService

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.NoSuchElementException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoServiceTest {
@@ -44,4 +46,72 @@ public class AlunoServiceTest {
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
     }
+
+    // #1 - Tenta salvar sem setar um numero de matricula
+    @Test
+    public void salvarSemMatricula() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Luis Felipe");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula(null);
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);
+        });
+    }
+
+    // #2 - Testa a funcionalidade de excluir cadastro de aluno
+    @Test
+    public void deletarAlunoExistente() {
+        Aluno aluno = new Aluno();
+        aluno.setId(2L);
+        aluno.setNome("Carlos");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("654321");
+
+        this.serviceAluno.save(aluno);
+        this.serviceAluno.deleteById(2L);
+
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+            this.serviceAluno.getById(2L);
+        });
+    }
+
+    // #3 - Testa a funcionalidade de atualizar cadastro de aluno
+    @Test
+    public void atualizarAluno() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Ana Maria");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("789101");
+
+        this.serviceAluno.save(aluno);
+
+        Aluno alunoSalvo = this.serviceAluno.getById(aluno.getId());
+        Assert.assertNotNull(alunoSalvo);
+
+        alunoSalvo.setNome("Ana Maria da Silva");
+        alunoSalvo.setCurso(Curso.CONTABILIDADE);
+        this.serviceAluno.save(alunoSalvo);
+
+        Aluno alunoAtualizado = this.serviceAluno.getById(alunoSalvo.getId());
+        Assert.assertEquals("Ana Maria da Silva", alunoAtualizado.getNome());
+        Assert.assertEquals(Curso.CONTABILIDADE, alunoAtualizado.getCurso());
+    }
+
+    // #4 - Testa o tratamento de exceções quando for realizado uma busca por um aluno com ID inexistente
+    @Test
+    public void buscarAlunoIdInexistente() {
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+            this.serviceAluno.getById(999L);
+        });
+    }
+
+
 }
