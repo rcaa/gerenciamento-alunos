3bd03c6bf9634298f4764d02e6ed2e50d6e7c2e8
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2024-12-16 22:08:47 -0300

    teste unitario em UsuarioRepositoryTest

--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,54 @@
 package br.com.gerenciamento.repository;
 
-import org.junit.runner.RunWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import br.com.gerenciamento.model.Usuario;
+
 @SpringBootTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Test
+    public void testFindByEmail() {
+        Usuario usuario = new Usuario();
+        usuario.setId(2L);
+        usuario.setEmail("jeff23@gmail.com");
+        usuario.setUser("JeffersonAlan");
+        usuario.setSenha("jeff112233");
+
+        this.usuarioRepository.save(usuario);
+
+        assertEquals("JeffersonAlan", usuarioRepository.findByEmail("jeff23@gmail.com").getUser());
+    }
+
+    @Test
+    public void testFindByEmail_NonExistentEmail() {
+        Usuario foundUsuario = usuarioRepository.findByEmail("naoexiste@example.com");
+        assertNull(foundUsuario);
+    }
+
+    @Test
+    public void buscarLogin(){
+        Usuario usuario = new Usuario();
+        usuario.setId(2L);
+        usuario.setEmail("jeff23@gmail.com");
+        usuario.setUser("JeffersonAlan");
+        usuario.setSenha("jeff112233");
+
+        this.usuarioRepository.save(usuario);
+
+        assertEquals("jeff23@gmail.com", usuarioRepository.buscarLogin("JeffersonAlan", "jeff112233").getEmail());
+    }
+
+    @Test
+    public void testBuscarLogin_InvalidCredentials() {
+        Usuario foundUsuario = usuarioRepository.buscarLogin("jefferson121123", "senhaErrada");
+        assertNull(foundUsuario);
+    }
 }
