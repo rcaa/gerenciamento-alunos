c6355c0dca1be7f80eac0f6a7ff5654fabdf8949
Otávio Timóteo <otavio.timoteo@ufape.edu.br>
2024-12-17 22:57:16 -0300

    Adicionando testes para a service de Aluno

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -5,6 +5,7 @@ import br.com.gerenciamento.enums.Status;
 import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
 import jakarta.validation.ConstraintViolationException;
+import java.util.NoSuchElementException;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,71 @@ public class AlunoServiceTest {
         aluno.setMatricula("123456");
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
+    }
+    
+        @Test
+    public void salvarAlunoComDadosValidos() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Carlos Silva");
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("20241234");
+
+        this.serviceAluno.save(aluno);
+
+        Aluno alunoRetorno = this.serviceAluno.getById(1L);
+        Assert.assertEquals("Carlos Silva", alunoRetorno.getNome());
+    }
+
+    @Test
+    public void naoSalvarAlunoSemNome() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("20241234");
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            serviceAluno.save(aluno);
+        });
+    }
+
+    @Test
+    public void atualizarStatusAluno() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Lucas Oliveira");
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.INATIVO);
+        aluno.setMatricula("20242345");
+        serviceAluno.save(aluno);
+
+        aluno.setStatus(Status.ATIVO);
+        this.serviceAluno.save(aluno);
+
+        Aluno alunoRetorno = this.serviceAluno.getById(1L);
+
+        Assert.assertEquals(Status.ATIVO, alunoRetorno.getStatus());
+    }
+
+    @Test
+    public void deletarAlunoPorId() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Fernanda Lima");
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("20243456");
+        
+        
+        this.serviceAluno.save(aluno);
+
+        Aluno alunoRetorno = this.serviceAluno.getById(1L);
+
+
+        serviceAluno.deleteById(alunoRetorno.getId());
+
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+            serviceAluno.getById(alunoRetorno.getId());
+        });
     }
 }
