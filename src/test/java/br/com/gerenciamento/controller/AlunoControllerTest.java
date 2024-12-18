3222c44e2f3dddfbaee971a9745aa32287ae3756
caiogmrocha <caioguilherme739@gmail.com>
2024-12-18 14:50:01 -0300

    test: garantir que o endpoint POST /InserirAlunos retorne 200 caso o aluno seja salvo corretamente

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,55 @@
 package br.com.gerenciamento.controller;
 
+import static org.junit.Assert.assertThat;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.service.ServiceAluno;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AlunoControllerTest {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void inserirAluno() {
+        // Cria um novo aluno para teste
+        Aluno novoAluno = new Aluno();
+        novoAluno.setNome("João Silva");
+        novoAluno.setMatricula("123456");
+        novoAluno.setCurso(Curso.INFORMATICA);
+        novoAluno.setStatus(Status.ATIVO);
+        novoAluno.setTurno(Turno.MATUTINO);
+
+        // Insere o aluno no banco de dados
+        ResponseEntity<String> response = testRestTemplate.postForEntity("/InsertAlunos", novoAluno, String.class);
+
+        // Verifica se o aluno foi inserido corretamente
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    // @Test
+    // public void listagemAlunos() {
+    // }
+
+    // @Test
+    // public void editar() {
+    // }
+
+    // @Test
+    // public void removerAluno() {
+    // }
 }
