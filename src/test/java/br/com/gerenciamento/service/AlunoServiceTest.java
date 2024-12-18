3b261673c9e8b0da6b4db56925895b92f7be2e2c
O Ala <141387803+allanjose001@users.noreply.github.com>
2024-12-18 16:40:57 -0300

    Implementado testes unitários de Service
    adicionado 4 testes unitários para AlunoServiceTest e UsuarioServiceTest

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -6,14 +6,45 @@ import br.com.gerenciamento.enums.Turno;
 import br.com.gerenciamento.model.Aluno;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoServiceTest {
+
+    private Aluno criarAlunoGenerico1() {
+        Aluno aluno = new Aluno();
+        aluno.setId(1L);
+        aluno.setNome("Vinicius");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+        return aluno;
+    }
+
+    private Aluno criarAlunoGenerico2() {
+        Aluno aluno = new Aluno();
+        aluno.setId(2L);
+        aluno.setNome("Jobson");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.INATIVO);
+        aluno.setMatricula("654321");
+        return aluno;
+    }
 
     @Autowired
     private ServiceAluno serviceAluno;
@@ -39,9 +70,53 @@ public class AlunoServiceTest {
         aluno.setId(1L);
         aluno.setTurno(Turno.NOTURNO);
         aluno.setCurso(Curso.ADMINISTRACAO);
-        aluno.setStatus(Status.ATIVO);
+        aluno.setStatus(Status.INATIVO);
         aluno.setMatricula("123456");
-        Assert.assertThrows(ConstraintViolationException.class, () -> {
+        assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
+    }
+
+    @Test
+    public void deleteById() {
+        Aluno aluno = criarAlunoGenerico1();
+        serviceAluno.save(aluno);
+
+        serviceAluno.deleteById(1L);
+        assertThrows(Exception.class, () -> serviceAluno.getById(1L));
+    }
+
+    @Test
+    public void findByNomeIgnoreCase() {
+        Aluno aluno1 = criarAlunoGenerico1();
+
+        serviceAluno.save(aluno1);
+
+        List<Aluno> alunosEncontrados = serviceAluno.findByNomeContainingIgnoreCase("VINICIUS");
+        assertEquals(1, alunosEncontrados.size());
+
+    }
+
+    @Test
+    public void findByStatusInativo() {
+        Aluno aluno1 = criarAlunoGenerico2();
+
+        serviceAluno.save(aluno1);
+
+        List<Aluno> alunosEncontrados = serviceAluno.findByStatusInativo();
+
+        assertEquals(1, alunosEncontrados.size());
+        assertEquals(Status.INATIVO, alunosEncontrados.get(0).getStatus());
+    }
+
+    @Test
+    public void findByStatusAtivo() {
+        Aluno aluno1 = criarAlunoGenerico1();
+
+        serviceAluno.save(aluno1);
+
+        List<Aluno> alunosEncontrados = serviceAluno.findByStatusAtivo();
+
+        assertEquals(1, alunosEncontrados.size());
+        assertEquals(Status.ATIVO, alunosEncontrados.get(0).getStatus());
     }
 }
--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,10 +1,92 @@
 package br.com.gerenciamento.service;
 
+import br.com.gerenciamento.exception.CriptoExistsException;
+import br.com.gerenciamento.exception.EmailExistsException;
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.repository.UsuarioRepository;
+import br.com.gerenciamento.util.Util;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioServiceTest {
+
+    public Usuario criarUsuarioGenerico() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("usuario@gmail.com");
+        usuario.setUser("usuario");
+        usuario.setSenha("1234");
+
+        return usuario;
+    }
+
+    @InjectMocks
+    private ServiceUsuario serviceUsuario;
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void salvarUsuario() throws Exception {
+        Usuario usuario = criarUsuarioGenerico();
+
+        when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(null);
+
+        serviceUsuario.salvarUsuario(usuario);
+
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuarioEmailDuplicado() {
+        Usuario usuario = criarUsuarioGenerico();
+
+        when(usuarioRepository.findByEmail(usuario.getEmail())).thenReturn(usuario);
+
+        assertThrows(EmailExistsException.class, () -> serviceUsuario.salvarUsuario(usuario));
+
+        verify(usuarioRepository, never()).save(any(Usuario.class));
+    }
+
+    @Test
+    public void salvarUsuarioFalhaSenha() throws Exception {
+        Usuario usuario = criarUsuarioGenerico();
+
+        mockStatic(Util.class);
+        when(Util.md5(usuario.getSenha())).thenThrow(NoSuchAlgorithmException.class);
+
+        assertThrows(CriptoExistsException.class, () -> serviceUsuario.salvarUsuario(usuario));
+
+        verify(usuarioRepository, never()).save(any(Usuario.class));
+    }
+
+    @Test
+    public void loginBemSucedido(){
+        Usuario usuario = criarUsuarioGenerico();
+
+        when(usuarioRepository.buscarLogin("usuario", "1234")).thenReturn(usuario);
+
+        Usuario resultado = serviceUsuario.loginUser("usuario", "1234");
+
+        assertNotNull(resultado);
+        assertEquals("usuario", resultado.getUser());
+        assertEquals("1234", resultado.getSenha());
+    }
 }
