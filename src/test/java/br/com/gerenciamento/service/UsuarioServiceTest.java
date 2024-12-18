fceb8209203b844886378d73e24dc9edec8637fc
caiogmrocha <caioguilherme739@gmail.com>
2024-12-17 22:51:30 -0300

    test: garantir que ServiceUsuario.loginUser() retorne uma instância de Usuario

--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -32,4 +32,23 @@ public class UsuarioServiceTest {
             this.serviceUsuario.salvarUsuario(user);
         });
     }
+
+    @Test
+    public void deveriaLogarUsuario() {
+        Usuario user = new Usuario();
+        user.setEmail("john@doe.com");
+        user.setSenha("1234");
+
+        try {
+            this.serviceUsuario.salvarUsuario(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            Assert.fail("Não deveria lançar exceção");
+        }
+
+        Usuario userRetorno = this.serviceUsuario.loginUser(user.getEmail(), user.getSenha());
+
+        Assert.assertNotNull(userRetorno);
+    }
 }
