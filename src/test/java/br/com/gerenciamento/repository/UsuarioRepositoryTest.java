6b1168e0c12856aae47a9ac7ed649e8cb870f035
Otávio Timóteo <otavio.timoteo@ufape.edu.br>
2024-12-17 22:58:01 -0300

    Adicionando testes para o repositório de Usuário

--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,79 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.service.ServiceUsuario;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+    
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    private ServiceUsuario serviceUsuario;
+    
+    
+    @Test
+    public void buscarUsuarioPorEmail() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("João");
+        usuario.setEmail("joao@example.com");
+        usuario.setSenha("123456");
+        
+        serviceUsuario.salvarUsuario(usuario);        
+        
+        Usuario usuarioEncontrado = usuarioRepository.findByEmail(usuario.getEmail());        
+        Assert.assertNotNull(usuarioEncontrado.getId());
+    }
+    
+    @Test
+    public void buscarUsuarioPorLogin() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("João");
+        usuario.setEmail("joao@example.com");
+        usuario.setSenha("123456");
+        
+        serviceUsuario.salvarUsuario(usuario);        
+        
+        Usuario usuarioEncontrado = usuarioRepository.buscarLogin(usuario.getUser(),usuario.getSenha());        
+        Assert.assertNotNull(usuarioEncontrado.getId());
+    }
+    
+       @Test
+    public void buscarUsuarioPorEmailNaoRegistrado() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("João");
+        usuario.setEmail("joao@example.com");
+        usuario.setSenha("123456");
+        
+        serviceUsuario.salvarUsuario(usuario);        
+        
+        Usuario usuarioEncontrado = usuarioRepository.findByEmail("pedro@example.com");        
+        Assert.assertNotNull(usuarioEncontrado.getId());
+    }
+    
+    @Test
+    public void buscarUsuarioPorLoginNaoRegistrado() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("João");
+        usuario.setEmail("joao@example.com");
+        usuario.setSenha("123456");
+        
+        serviceUsuario.salvarUsuario(usuario);        
+        
+        Usuario usuarioEncontrado = usuarioRepository.buscarLogin("pedro",usuario.getSenha());        
+        Assert.assertNotNull(usuarioEncontrado.getId());
+    }
+    
 }
