16bbd272cdb38abc4bc5e0e77912db78c0e2cd60
O Ala <141387803+allanjose001@users.noreply.github.com>
2024-12-18 16:43:09 -0300

    Implementado testes de integração para controller
    Implementados 4 testes de integração AlunoController e UsuarioController

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,106 @@
 package br.com.gerenciamento.controller;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
 
-@RunWith(SpringRunner.class)
+import br.com.gerenciamento.service.ServiceAluno;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AlunoControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private ServiceAluno serviceAluno;
+
+    @InjectMocks
+    private AlunoController alunoController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(alunoController)
+                .setValidator(new LocalValidatorFactoryBean())
+                .build();
+    }
+
+    @Test
+    public void ListagemAlunos() throws Exception {
+        mockMvc.perform(get("/alunos-adicionados"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/listAlunos"));
+    }
+
+    @Test
+    public void pesquisarAluno() throws Exception {
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("biruleibe");
+        aluno1.setMatricula("001");
+        aluno1.setCurso(Curso.ADMINISTRACAO);
+        aluno1.setStatus(Status.ATIVO);
+        aluno1.setTurno(Turno.NOTURNO);
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Tanajura");
+        aluno2.setMatricula("002");
+        aluno2.setCurso(Curso.BIOMEDICINA);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setTurno(Turno.MATUTINO);
+
+        when(serviceAluno.findByNomeContainingIgnoreCase("Tanajura")).thenReturn(List.of(aluno2));
+
+        mockMvc.perform(post("/pesquisar-aluno")
+                .param("nome", "Tanajura"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("ListaDeAlunos"))
+                .andExpect(model().attribute("ListaDeAlunos", hasItem(hasProperty("nome", is("Tanajura")))));
+    }
+
+    @Test
+    void testRemoverAluno() throws Exception {
+        Long alunoId = 1L;
+        mockMvc.perform(get("/remover/{id}", alunoId))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/alunos-adicionados"));
+    }
+
+    @Test
+    void SalvarAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Aristeu");
+        aluno.setMatricula("001");
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.NOTURNO);
+
+        Mockito.doNothing().when(serviceAluno).save(Mockito.any(Aluno.class));
+
+        mockMvc.perform(post("/InsertAlunos")
+                .flashAttr("aluno", aluno))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/alunos-adicionados"));
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,98 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.service.ServiceUsuario;
+import br.com.gerenciamento.util.Util;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-@RunWith(SpringRunner.class)
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private ServiceUsuario serviceUsuario;
+
+    @InjectMocks
+    private UsuarioController usuarioController;
+
+    @BeforeEach
+    public void setUp() { mockMvc = MockMvcBuilders.standaloneSetup(usuarioController)
+                .build(); }
+
+    @Test
+    public void loginUsuario() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("usuario");
+        usuario.setSenha("12345");
+
+        // Configura o mock para retornar o usuário válido
+        when(serviceUsuario.loginUser(anyString(), anyString())).thenReturn(usuario);
+
+        mockMvc.perform(post("/login")
+                .flashAttr("usuario", usuario))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home/index")) // Confirma que redireciona para a página inicial
+                .andExpect(model().attributeExists("usuario")); // Verifica se o modelo tem "aluno"
+    }
+
+    @Test
+    public void salvarUsuario() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("usuario@gmail.com");
+        usuario.setUser("usuario");
+        usuario.setSenha("12345");
+
+        mockMvc.perform(post("/salvarUsuario")
+                .flashAttr("usuario", usuario))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+    }
+
+    @Test
+    void cadastrarNovoUsuario() throws Exception {
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setUser("novoUsuario");
+        novoUsuario.setSenha("54321");
+
+        doNothing().when(serviceUsuario).salvarUsuario(any(Usuario.class));
+
+        mockMvc.perform(post("/salvarUsuario")
+                .flashAttr("usuario", novoUsuario))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+    @Test
+    void logoutUsuario() throws Exception {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("usuarioLogado", new Usuario());
+
+        mockMvc.perform(post("/logout").session(session))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"));
+    }
 }
