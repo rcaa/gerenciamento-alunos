844cc72aa12c30cfa726ed88ecf6e46e56b50ce6
biancacneves <bianca.neves@ufape.edu.br>
2024-12-18 16:14:23 -0300

    Implementa 4 testes de integração em AlunoControllerTest

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,32 @@
-package br.com.gerenciamento.controller;
-
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class AlunoControllerTest {
+@AutoConfigureMockMvc
+class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deveRetornarTodosOsAlunos() throws Exception {
+        mockMvc.perform(get("/alunos"))
+               .andExpect(status().isOk())
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+               .andExpect(jsonPath("$.length()").isNotEmpty());
+    }
+
+    @Test
+    void deveCriarUmNovoAluno() throws Exception {
+        String novoAlunoJson = """
+        {
+            "nome": "Bianca C Neves",
+            "idade": 23,
+            "curso": "BCC"
+        }
+        """;
+
+        mockMvc.perform(post("/alunos")
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(novoAlunoJson))
+               .andExpect(status().isCreated())
+               .andExpect(jsonPath("$.nome").value("João Silva"));
+    }
 }
