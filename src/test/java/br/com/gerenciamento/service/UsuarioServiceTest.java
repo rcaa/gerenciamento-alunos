3981a77d7d885263080d0b4959ca2a8f45b43a96
caiogmrocha <caioguilherme739@gmail.com>
2024-12-17 22:04:16 -0300

    test: garantir que ServiceUsuario.salvarUsuario() lance EmailExistsException caso o e-mail provido já esteja cadastrado

--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,10 +1,35 @@
 package br.com.gerenciamento.service;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.exception.EmailExistsException;
+import br.com.gerenciamento.model.Usuario;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioServiceTest {
+    @Autowired
+    private ServiceUsuario serviceUsuario;
+
+    @Test
+    public void deveriaLancarEmailExistsException() {
+        Usuario user = new Usuario();
+        user.setEmail("john@doe.com");
+        user.setSenha("1234");
+
+        try {
+            this.serviceUsuario.salvarUsuario(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Assert.assertThrows(EmailExistsException.class, () -> {
+            this.serviceUsuario.salvarUsuario(user);
+        });
+    }
 }
