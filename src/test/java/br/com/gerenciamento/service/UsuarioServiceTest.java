a6062e716748363f246aa7bd6d761f58f09bbc6f
PacssauroRex <victorcauaorg@gmail.com>
2024-12-18 01:27:41 -0300

    Implementação dos testes para Usuario Service

--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,10 +1,74 @@
 package br.com.gerenciamento.service;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.util.Util;
+import jakarta.validation.ConstraintViolationException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioServiceTest {
+
+    @Autowired
+    private ServiceUsuario serviceUsuario;
+
+    @Test
+    public void salvarUsuario() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setEmail("okarun@gmail.com");
+        usuario.setUser("Okarun");
+        usuario.setSenha("123");
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        Assert.assertEquals("Okarun", usuario.getUser());
+
+    }
+
+    @Test
+    public void realizarLogin() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setEmail("okarun@gmail.com");
+        usuario.setUser("Okarun");
+        usuario.setSenha("123");
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        Usuario usuarioLogado = this.serviceUsuario.loginUser("Okarun", Util.md5("123"));
+        Assert.assertNotNull(usuarioLogado);
+
+    }
+
+    @Test
+    public void loginInvalido() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setEmail("okarun@gmail.com");
+        usuario.setUser("Okarun");
+        usuario.setSenha("123");
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        Usuario usuarioLogado = this.serviceUsuario.loginUser("Okarun", Util.md5("momo"));
+        Assert.assertNull(usuarioLogado);
+
+    }
+
+    @Test
+    public void salvarSemEmail() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setUser("Okarun");
+        usuario.setSenha("123");
+        this.serviceUsuario.salvarUsuario(usuario);
+        
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceUsuario.salvarUsuario(usuario);});
+    }
+
 }
